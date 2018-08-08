ba7e00c6af914d122ddeb74637f059488fd988bb
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2018-08-08 16:52:08 +0200

    STAC-2493 Remove snmp component exclusion from agent build. Add image build step

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -31,8 +31,7 @@ build do
   env = with_embedded_path(env)
 
   # we assume the go deps are already installed before running omnibus
-  # TODO pass build-exclude tru env variable
-  command "invoke -e agent.build --rebuild --use-embedded-libs --no-development --build-exclude=snmp", env: env
+  command "invoke -e agent.build --rebuild --use-embedded-libs --no-development", env: env
   if windows?
     command "invoke -e systray.build --rebuild --use-embedded-libs --no-development", env: env
   end
