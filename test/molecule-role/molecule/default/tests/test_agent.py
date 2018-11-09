551f766eee898b982dc8bd68d5f97e322864a857
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2018-11-09 11:00:54 +0100

    STAC-2599 Downgrade pytest due to https://github.com/pytest-dev/pytest/issues/4304

--- test/molecule-role/molecule/default/tests/test_agent.py
+++ test/molecule-role/molecule/default/tests/test_agent.py
@@ -5,6 +5,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('agent_vm')
 
 
-def test_opt_stackstate_directory(host):
-    f = host.file('/opt/stackstate-agent/')
-    assert f.is_directory
+def test_stackstate_agent_is_installed(host):
+    agent = host.package("stackstate-agent")
+    assert agent.is_installed
+    # TODO
+    # assert agent.version.startswith("2")
+
+
+def test_stackstate_agent_running_and_enabled(host):
+    agent = host.service("stackstate-agent")
+    assert agent.is_running
+    assert agent.is_enabled
