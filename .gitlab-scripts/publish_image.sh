e24c58060be29bf4282cb860dc9ed74d2930eb1c
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2018-11-12 16:35:48 +0100

    STAC-2599 Add publish image on release stage. Use `inv version` to get correct version for image

--- .gitlab-scripts/publish_image.sh
+++ .gitlab-scripts/publish_image.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 TAG=$1
-cp ${@:2} Dockerfiles/agent
-docker build -t stackstate/stackstate-agent:$TAG -t stackstate/stackstate-agent:latest Dockerfiles/agent
+REPO=$2
+
+cp ${@:3} Dockerfiles/agent
+
+docker build -t stackstate/$REPO:$TAG -t stackstate/$REPO:latest Dockerfiles/agent
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push stackstate/stackstate-agent:$TAG
 docker push stackstate/stackstate-agent:latest
