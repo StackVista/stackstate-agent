8f0ba1f748cb4320d10cd2d0f364b0244973e9e4
Bram Schuur <bramschuur@gmail.com>
2018-11-13 17:46:01 +0100

    Fix linting

--- test/molecule-role/molecule/default/tests/test_receiver.py
+++ test/molecule-role/molecule/default/tests/test_receiver.py
@@ -18,21 +18,32 @@ def test_docker_compose_file(host):
 
 
 def test_created_connection(host):
-    #def wait_for_connection():
-        data = host.check_output(
-            "curl http://localhost:7070/api/topic/sts_correlate_endpoints?limit=1000")
+    url = "http://localhost:7070/api/topic/sts_correlate_endpoints?limit=1000"
+
+    def wait_for_connection():
+        data = host.check_output("curl %s" % url)
         json_data = json.loads(data)
-        outgoing = next(record for record in json_data["messages"] if record["message"]["Connection"]["remoteEndpoint"]["endpoint"]["port"] == 125)
+        outgoing = next(record
+                        for record
+                        in json_data["messages"]
+                        if record["message"]["Connection"]
+                        ["remoteEndpoint"]["endpoint"]["port"] == 125
+                        )
         outgoing_conn = outgoing["message"]["Connection"]
         print outgoing_conn
         # TODO: Enable this once we use a .deb with directional relations
         # assert outgoing_conn["direction"] == "OUTGOING"
         assert outgoing_conn["connectionType"] == "TCP"
-        incoming = next(record for record in json_data["messages"] if record["message"]["Connection"]["localEndpoint"]["endpoint"]["port"] == 125)
+        incoming = next(record
+                        for record
+                        in json_data["messages"]
+                        if record["message"]["Connection"]
+                        ["localEndpoint"]["endpoint"]["port"] == 125
+                        )
         incoming_conn = incoming["message"]["Connection"]
         print incoming_conn
         # TODO: Enable this once we use a .deb with directional relations
         # assert incoming_conn["direction"] == "INCOMING"
         assert incoming_conn["connectionType"] == "TCP"
 
-    #util.wait_until(wait_for_connection, 30, 3)
+    util.wait_until(wait_for_connection, 30, 3)
