7187aa043fa1fd07366b0de70561f3e23ab2790d
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2018-11-13 14:41:31 +0100

    STAC-2599 Make sure url safe version is used

--- omnibus/package-scripts/publish_package.sh
+++ omnibus/package-scripts/publish_package.sh
@@ -13,7 +13,7 @@ fi
 
 if [ -z ${STACKSTATE_AGENT_VERSION+x} ]; then
 	# Pick the latest tag by default for our version.
-	STACKSTATE_AGENT_VERSION=$(inv version)
+	STACKSTATE_AGENT_VERSION=$(inv version -u)
 	# But we will be building from the master branch in this case.
 fi
 echo $STACKSTATE_AGENT_VERSION
