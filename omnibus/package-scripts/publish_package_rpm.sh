1ece3eec2e434496cc69b18c14b624c76353f288
Vyacheslav Voronenko <git@voronenko.info>
2018-12-11 18:23:45 +0100

    merging artifacts into existing repo

---
+++ omnibus/package-scripts/publish_package_rpm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TARGET_BUCKET=$1
+
+CODENAME=${2:-$CI_COMMIT_REF_NAME}
+TARGET_CODENAME=${CODENAME:-dirty}
+
+
+if [ -z ${TARGET_BUCKET+x} ]; then
+	echo "Missing S3 bucket parameter"
+	exit 1;
+fi
+
+if [ -z ${STACKSTATE_AGENT_VERSION+x} ]; then
+	# Pick the latest tag by default for our version.
+	STACKSTATE_AGENT_VERSION=$(inv version -u)
+	# But we will be building from the master branch in this case.
+fi
+echo $STACKSTATE_AGENT_VERSION
+
+rpm-s3 -b $TARGET_BUCKET -p "${TARGET_CODENAME}" $CI_PROJECT_DIR/outcomes/pkg/*.rpm
