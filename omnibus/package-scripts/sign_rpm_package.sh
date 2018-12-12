76bd7c1e45595081334d9dd42a36449df30d5463
vk7282 <vishalkmr.mfp@gmail.com>
2018-12-12 18:20:22 +0530

    after signing verify if signed

--- omnibus/package-scripts/sign_rpm_package.sh
+++ omnibus/package-scripts/sign_rpm_package.sh
@@ -11,8 +11,6 @@ fi
 
 echo $STACKSTATE_AGENT_VERSION
 
-printenv
-
 echo "$SIGNING_PUBLIC_KEY" | gpg --import
 echo "$SIGNING_PRIVATE_KEY" > gpg_private.key
 echo "$SIGNING_PRIVATE_PASSPHRASE" | gpg --batch --yes --passphrase-fd 0 --import gpg_private.key
@@ -20,44 +18,22 @@ echo "$SIGNING_KEY_ID"
 
 ls $CI_PROJECT_DIR/outcomes/pkg/*.*
 
-# Step: 1
 # Export your public key from your key ring to a text file.
-#
-# You will use the information for Real Name and Email you used to
-# create your key.
-
 gpg --export -a 'StackState' > RPM-GPG-KEY-stackstate
 
-# Step: 4
 # Import your public key to your RPM DB
-#
-# If you plan to share your custom built RPM packages with others, make sure
-# to have your public key file available online so others can verify RPMs
-
 rpm --import RPM-GPG-KEY-stackstate
 
-# Step: 5
 # Verify the list of gpg public keys in RPM DB
-
 rpm -q gpg-pubkey --qf '%{name}-%{version}-%{release} --> %{summary}\n'
-gpg --list-secret-keys --with-fingerprint --with-colons
-# Step: 6
+
 # Configure your ~/.rpmmacros file
 # %_gpg_name  => Use the Real Name you used to create your key
-
 echo "%_gpg_name StackState <info@stackstate.com>" > ~/.rpmmacros
 
-gpg-agent --daemon
-cat <<EOF >~/.gnupg/gpg-agent.conf
-default-cache-ttl 46000
-allow-preset-passphrase
-EOF
-
-gpg-connect-agent RELOADAGENT /bye
-echo $SIGNING_PRIVATE_PASSPHRASE | /usr/libexec/gpg-preset-passphrase -v -c $(gpg --list-secret-keys --with-fingerprint --with-colons | awk -F: '$1 == "uid" { print $8 }')
-
-# Step: 7
 # Sign your custom RPM package
 chmod +x rpm-sign
 ./rpm-sign $SIGNING_PRIVATE_PASSPHRASE $CI_PROJECT_DIR/outcomes/pkg/*.rpm
-#rpm --addsign $CI_PROJECT_DIR/outcomes/pkg/*.rpm
+
+# Check the signature to make sure it was signed
+rpm --checksig $CI_PROJECT_DIR/outcomes/pkg/*.rpm
