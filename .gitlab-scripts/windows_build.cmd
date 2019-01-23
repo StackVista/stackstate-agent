e962dcee461f422e9626933ee2ece576255d47ef
Vyacheslav Voronenko <git@voronenko.info>
2019-01-23 09:37:58 +0100

    gitlab wrapped build step

--- .gitlab-scripts/windows_build.cmd
+++ .gitlab-scripts/windows_build.cmd
@@ -1,11 +1,16 @@
 REM set WIN_CI_PROJECT_DIR=%CD%
 REM set WORKON_HOME=%WIN_CI_PROJECT_DIR%
 
+
+echo call %WORKON_HOME%\venv\Scripts\activate.bat
+call %WORKON_HOME%\venv\Scripts\activate.bat
+call ridk enable
+
 set
 
 dir
 
-echo call %WORKON_HOME%\venv\Scripts\activate.bat
-call %WORKON_HOME%\venv\Scripts\activate.bat
+cd %GOPATH%\src\github.com\StackVista\stackstate-agent
+echo cd %GOPATH%\src\github.com\StackVista\stackstate-agent
 
-
+inv -e agent.omnibus-build --skip-sign --log-level debug --skip-deps
