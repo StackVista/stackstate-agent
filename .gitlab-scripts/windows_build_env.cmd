024484fada9a7c37305c1c9dedf4a95f70b5876a
Vyacheslav Voronenko <git@voronenko.info>
2019-01-25 11:24:24 +0100

    prefer linking over copying on cd instance

--- .gitlab-scripts/windows_build_env.cmd
+++ .gitlab-scripts/windows_build_env.cmd
@@ -11,11 +11,11 @@ if exist \omnibus-ruby rd /s/q \omnibus-ruby
 if exist %OMNIBUS_BASE_DIR_WIN% rd /s/q %OMNIBUS_BASE_DIR_WIN%
 if exist \opt\stackstate-agent rd /s/q \opt\stackstate-agent
 if exist %GOPATH%\src\github.com\StackVista\stackstate-agent rd /s/q %GOPATH%\src\github.com\StackVista\stackstate-agent
-mkdir %GOPATH%\src\github.com\StackVista\stackstate-agent
-xcopy /q/h/e/s * %GOPATH%\src\github.com\StackVista\stackstate-agent
+REM mkdir %GOPATH%\src\github.com\StackVista\stackstate-agent
+REM xcopy /q/h/e/s * %GOPATH%\src\github.com\StackVista\stackstate-agent
+mklink /J %GOPATH%\src\github.com\StackVista\stackstate-agent %WIN_CI_PROJECT_DIR%
 cd %GOPATH%\src\github.com\StackVista\stackstate-agent
 call mkvirtualenv venv
 cd %GOPATH%\src\github.com\StackVista\stackstate-agent
 echo cd %GOPATH%\src\github.com\StackVista\stackstate-agent
 pip install -r requirements.txt
-
