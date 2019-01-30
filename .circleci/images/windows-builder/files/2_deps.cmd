37b22458099069f7fda791c465ebfb670bf3bb3a
Vyacheslav Voronenko <git@voronenko.info>
2019-01-30 13:07:28 +0100

    improve(pr comments) applied pr comments

--- .circleci/images/windows-builder/files/2_deps.cmd
+++
@@ -1,9 +0,0 @@
-set OMNIBUS_BASE_DIR_WIN=c:\omni-base\manual
-set OMNIBUS_BASE_DIR_WIN_OMNIBUS=c:/omni-base/manual
-
-set WIN_CI_PROJECT_DIR=c:\workspaces\builds\ToVYBrDV\0\stackvista\stackstate-agent\
-set WORKON_HOME=%GOPATH%\src\github.com\StackVista\stackstate-agent
-
-cd %WORKON_HOME%
-workon venv
-inv -e deps
