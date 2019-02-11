8d8accba6538f056f4a6f41c71fe9ef2b149aa51
Vyacheslav Voronenko <git@voronenko.info>
2019-02-11 16:45:36 +0100

    toolset needed for customaction

--- .gitlab-scripts/windows_build.cmd
+++ .gitlab-scripts/windows_build.cmd
@@ -5,6 +5,7 @@ REM set WORKON_HOME=%WIN_CI_PROJECT_DIR%
 echo call %WORKON_HOME%\venv\Scripts\activate.bat
 call %WORKON_HOME%\venv\Scripts\activate.bat
 call ridk enable
+call %VCINSTALLDIR%\Common7\Tools\VsDevCmd.bat
 
 set
 
