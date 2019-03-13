8c281da6a2f17b18be92d5d2382660f2131554d1
Zandre Witte <zandre.witte@gmail.com>
2019-03-13 12:24:14 +0100

    added process filtering test to molecule

--- test/molecule-role/molecule/default/tests/test_receiver.py
+++ test/molecule-role/molecule/default/tests/test_receiver.py
@@ -293,15 +293,15 @@ def test_topology_components(host):
 
         # assert that process filtering works correctly
         # fedora specific process filtering
-        assert _component_data("process", "urn:process:/agent-fedora", "/usr/sbin/sshd") == None
-        assert _component_data("process", "urn:process:/agent-fedora", "/usr/sbin/dhclient") == None
-        assert _component_data("process", "urn:process:/agent-fedora", "/usr/lib/systemd/systemd-journald") == None
+        assert _component_data("process", "urn:process:/agent-fedora", "/usr/sbin/sshd") is None
+        assert _component_data("process", "urn:process:/agent-fedora", "/usr/sbin/dhclient") is None
+        assert _component_data("process", "urn:process:/agent-fedora", "/usr/lib/systemd/systemd-journald") is None
         # ubuntu specific process filtering
-        assert _component_data("process", "urn:process:/agent-ubuntu", "/usr/sbin/sshd") == None
-        assert _component_data("process", "urn:process:/agent-ubuntu", "/lib/systemd/systemd-journald") == None
-        assert _component_data("process", "urn:process:/agent-ubuntu", "/sbin/agetty") == None
+        assert _component_data("process", "urn:process:/agent-ubuntu", "/usr/sbin/sshd") is None
+        assert _component_data("process", "urn:process:/agent-ubuntu", "/lib/systemd/systemd-journald") is None
+        assert _component_data("process", "urn:process:/agent-ubuntu", "/sbin/agetty") is None
         # windows specific process filtering
-        assert _component_data("process", "urn:process:/agent-win", "C:\\Windows\\system32\\svchost.exe") == None
-        assert _component_data("process", "urn:process:/agent-win", "winlogon.exe") == None
-        assert _component_data("process", "urn:process:/agent-win", "C:\\Windows\\system32\\wlms\\wlms.exe") == None
+        assert _component_data("process", "urn:process:/agent-win", "C:\\Windows\\system32\\svchost.exe") is None
+        assert _component_data("process", "urn:process:/agent-win", "winlogon.exe") is None
+        assert _component_data("process", "urn:process:/agent-win", "C:\\Windows\\system32\\wlms\\wlms.exe") is None
     util.wait_until(wait_for_components, 30, 3)
