b3a29328870ce28da92d00b44d952062e17a9c81
Zandre Witte <zandre.witte@gmail.com>
2019-03-28 22:52:02 +0100

    fixes for keyerror in test receiver

--- test/molecule-role/molecule/default/tests/test_receiver.py
+++ test/molecule-role/molecule/default/tests/test_receiver.py
@@ -280,7 +280,7 @@ def test_topology_components(host):
                 if "TopologyComponent" in p and p["TopologyComponent"]["typeName"] == type_name and p["TopologyComponent"]["externalId"].startswith(external_id_prefix):
                     component_data = json.loads(p["TopologyComponent"]["data"])
                     if command:
-                        if len(component_data["command"]["args"]) > 0:
+                        if "args" in component_data["command"]:
                             if component_data["command"]["args"][0] == command:
                                 return component_data
                     else:
