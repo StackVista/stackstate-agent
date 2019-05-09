5908f2608b258bacc0589fd9e38da8936103d173
Vyacheslav Voronenko <git@voronenko.info>
2019-05-09 11:20:56 +0200

    removing link using junction

--- .gitlab-scripts/windows_build_env.cmd
+++ .gitlab-scripts/windows_build_env.cmd
@@ -10,13 +10,15 @@ mkdir .omnibus\pkg
 if exist \omnibus-ruby rd /s/q \omnibus-ruby
 if exist %OMNIBUS_BASE_DIR_WIN% rd /s/q %OMNIBUS_BASE_DIR_WIN%
 if exist \opt\stackstate-agent rd /s/q \opt\stackstate-agent
-if exist %GOPATH%\src\github.com\StackVista\stackstate-agent rd /s/q %GOPATH%\src\github.com\StackVista\stackstate-agent
-REM mkdir %GOPATH%\src\github.com\StackVista\stackstate-agent
-REM xcopy /q/h/e/s * %GOPATH%\src\github.com\StackVista\stackstate-agent
+echo "======================> cleaning sym link %GOPATH%\src\github.com\StackVista\stackstate-agent"
+REM if exist %GOPATH%\src\github.com\StackVista\stackstate-agent rd /s/q %GOPATH%\src\github.com\StackVista\stackstate-agent
+if exist %GOPATH%\src\github.com\StackVista\stackstate-agent junction -d "%GOPATH%\src\github.com\StackVista\stackstate-agent"
 mkdir -p c:\gopath\src\github.com\StackVista\
+rd /s/q %GOPATH%\src\github.com\StackVista\stackstate-agent
 mklink /J %GOPATH%\src\github.com\StackVista\stackstate-agent %WIN_CI_PROJECT_DIR%
 cd %GOPATH%\src\github.com\StackVista\stackstate-agent
 IF EXIST %GOPATH%\src\github.com\StackVista\stackstate-agent\venv GOTO VENV_EXIST
+echo "======================> cleaning virtual env"
 call mkvirtualenv venv
 cd %GOPATH%\src\github.com\StackVista\stackstate-agent
 echo cd %GOPATH%\src\github.com\StackVista\stackstate-agent
--- .gitlab-scripts/windows_deps.cmd
+++ .gitlab-scripts/windows_deps.cmd
@@ -10,6 +10,8 @@ call %WORKON_HOME%\venv\Scripts\activate.bat
 
 IF EXIST %GOPATH%\src\github.com\StackVista\stackstate-agent\vendor GOTO VENDOR_EXIST
 
+echo "======================> stackstate-agent\vendor missing, getting deps"
+
 inv -e deps
 
 :VENDOR_EXIST
