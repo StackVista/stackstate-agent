18c2f4c3a49c8c27b23b0991a4faff903fa300d4
Vyacheslav Voronenko <git@voronenko.info>
2019-05-31 11:38:16 +0200

    improve(manifests) example to spin up test ecs cluster and agent daemon service

---
+++ Dockerfiles/manifests/aws-ecs/tf-agent/variables.tf
@@ -0,0 +1,40 @@
+variable "ecs_cluster_name" {
+  description = "ECS Cluster Name"
+  default = "ecs-default"
+}
+
+variable "sts_agent_task_family" {
+  description = "Sts agent family"
+  default = "stackstate-agent-task"
+}
+
+variable "sts_agent_image" {
+  description = "version of agent to execute"
+  default = "stackstate/stackstate-agent-2-test:2.0.2.git.482.db7dccee"
+}
+
+variable "STS_URL" {
+  description = "Stackstate server url"
+#  default = "https://stac-4493-test-ecs.test.stackstate.io/stsAgent"
+  default = "http://eedd1ede.ngrok.io/stsAgent"
+}
+
+variable "STS_API_KEY" {
+  description = "Api key"
+}
+
+variable "STS_SKIP_SSL_VALIDATION" {
+  description = "Skip ssl validation"
+  default = "True"
+}
+
+variable "STS_PROCESS_AGENT_ENABLED" {
+  description = "Enables process agent"
+  default = "True"
+}
+
+ variable "STS_LOG_LEVEL" {
+   description = "Log level"
+   default = "debug"
+ }
+
