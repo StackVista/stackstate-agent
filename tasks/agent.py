e585069d9f4626d702ccaa094c2892c08c0643f7
Zandre Witte <zandre.witte@gmail.com>
2019-08-09 13:08:37 +0200

    sed rename fixes

--- tasks/agent.py
+++ tasks/agent.py
@@ -152,13 +152,10 @@ def apply_branding(ctx):
     sts_lower_replace = 's/datadog/stackstate/g'
 
     # Dist config templates
-    do_sed_rename(ctx, 's|## Learn more about tagging: https://docs.datadoghq.com/tagging/||', "./cmd/agent/dist/conf.d/containerd.d/conf.yaml.example")
-    do_sed_rename(ctx, 's|## Learn more about tagging: https://docs.datadoghq.com/tagging/||', "./cmd/agent/dist/conf.d/cri.d/conf.yaml.example")
-    do_sed_rename(ctx, lower_replace, "./cmd/agent/dist/conf.d/go_expvar.d/agent_stats.yaml.example")
-    do_sed_rename(ctx, 's|http://docs.datadoghq.com/integrations/java/|StackState Agent StackPack - Java JMX integration page|g', "./cmd/agent/dist/conf.d/jmx.d/conf.yaml.example")
-    do_sed_rename(ctx, lower_replace, "./cmd/agent/dist/conf.d/apm.yaml.default")
+    do_sed_rename(ctx, sts_lower_replace, "./cmd/agent/dist/conf.d/go_expvar.d/agent_stats.yaml.example")
+    do_sed_rename(ctx, sts_lower_replace, "./cmd/agent/dist/conf.d/apm.yaml.default")
     do_sed_rename(ctx, 's/dd/sts/g', "./cmd/agent/dist/dd-agent")
-    do_sed_rename(ctx, lower_replace, "./cmd/agent/dist/dd-agent")
+    do_sed_rename(ctx, sts_lower_replace, "./cmd/agent/dist/dd-agent")
 
     # Hardcoded checks and metrics
     do_sed_rename(ctx, sts_lower_replace, "./pkg/aggregator/aggregator.go")
