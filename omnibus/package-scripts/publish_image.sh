ab23730360a3d5f1c3d508ceb7069b20b3c088d0
Scott Crooks <scott.crooks@gmail.com>
2019-08-30 11:23:55 +0200

    Adding explicit 'docker.io' entry for registry

--- omnibus/package-scripts/publish_image.sh
+++ omnibus/package-scripts/publish_image.sh
@@ -7,17 +7,18 @@ IMAGE_REPO="${2}"
 DOCKERFILE_PATH="${3}"
 PUSH_LATEST="${4:-false}"
 REGISTRY="${5:-docker.io}"
+ORGANIZATION="${6:-stackstate}"
 
 echo "${IMAGE_TAG}"
 echo "${IMAGE_REPO}"
 echo "${DOCKERFILE_PATH}"
 
-docker build -t "${REGISTRY}/stackstate/${IMAGE_REPO}:${IMAGE_TAG}" "${DOCKERFILE_PATH}"
+docker build -t "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}" "${DOCKERFILE_PATH}"
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}" "${REGISTRY}"
-docker push "${REGISTRY}/stackstate/${IMAGE_REPO}:${IMAGE_TAG}"
+docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
 
 if [ "$PUSH_LATEST" = "true" ]; then
-    docker tag "${REGISTRY}/stackstate/${IMAGE_REPO}:${IMAGE_TAG}" "${REGISTRY}/stackstate/${IMAGE_REPO}:latest"
+    docker tag "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}" "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:latest"
     echo 'Pushing release to latest'
-    docker push "${REGISTRY}/stackstate/${IMAGE_REPO}:latest"
+    docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:latest"
 fi
