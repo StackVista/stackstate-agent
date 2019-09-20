7cb39595748f8e1598290e46b1a3f845001bd2a1
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2019-09-20 12:11:56 +0200

    test(openshift/k8s) [STAC-5376] make rbac part of kustomization base. Fix kustomazation overlay template with correct receiver url. Cleanup prepare

--- test/molecule-role/molecule/kubernetes/tests/test_kubernetes_topology.py
+++ test/molecule-role/molecule/kubernetes/tests/test_kubernetes_topology.py
@@ -12,3 +12,19 @@ def test_receiver_healthy(host):
         assert host.check_output(c) == "200"
 
     util.wait_until(assert_healthy, 100, 5)
+
+
+def test_node_agent_healthy(host):
+    def assert_healthy():
+        c = "kubectl wait --for=condition=Running daemonset/stackstate-agent --all --timeout=3s"
+        assert host.run(c).rc == 0
+
+    util.wait_until(assert_healthy, 100, 5)
+
+
+def test_cluster_agent_healthy(host):
+    def assert_healthy():
+        c = "kubectl wait --for=condition=Running deployment/stackstate-cluster-agent --all --timeout=3s"
+        assert host.run(c).rc == 0
+
+    util.wait_until(assert_healthy, 100, 5)
