3b925486df63af394c35540b3e11e114e8cad0cf
Zandre Witte <zandre.witte@gmail.com>
2020-06-18 10:11:59 +0200

    wip

--- test/molecule-role/molecule/vms/tests/test_receiver_topology.py
+++ test/molecule-role/molecule/vms/tests/test_receiver_topology.py
@@ -88,3 +88,44 @@ def test_dnat(host, common_vars):
             external_id_assert_fn=lambda v: service_to_proc_id_match.findall(v))["incoming"]["ip"] == ubuntu_private_ip
 
     util.wait_until(wait_for_components, 30, 3)
+
+
+def test_dnat(host, common_vars):
+    url = "http://localhost:7070/api/topic/sts_topo_process_agents?offset=0&limit=1000"
+
+    ubuntu_private_ip = _get_instance_config("agent-ubuntu")["private_address"]
+    fedora_private_ip = _get_instance_config("agent-fedora")["private_address"]
+    dnat_service_port = int(common_vars["dnat_service_port"])
+    dnat_server_port = int(common_vars["dnat_server_port"])
+
+    def wait_for_components():
+        data = host.check_output("curl \"%s\"" % url)
+        json_data = json.loads(data)
+        with open("./topic-topo-process-agents-dnat.json", 'w') as f:
+            json.dump(json_data, f, indent=4)
+
+        endpoint_match = re.compile("urn:endpoint:/.*:{}".format(ubuntu_private_ip))
+        endpoint = _find_component(
+            json_data=json_data,
+            type_name="endpoint",
+            external_id_assert_fn=lambda v: endpoint_match.findall(v))
+        assert json.loads(endpoint["data"])["ip"] == ubuntu_private_ip
+        endpoint_component_id = endpoint["externalId"]
+
+        proc_to_proc_id_match = re.compile("TCP:/urn:process:/agent-fedora:.*:.*->{}:{}".format(endpoint_component_id, dnat_service_port))
+        proc_to_service_id_match = re.compile("TCP:/urn:process:/agent-fedora:.*:.*->urn:process:/agent-ubuntu:.*:.*:{}:{}".format(ubuntu_private_ip, dnat_server_port))
+        service_to_proc_id_match = re.compile("TCP:/{}:{}->urn:process:/agent-ubuntu:.*:.*:{}:{}".format(endpoint_component_id, dnat_service_port, ubuntu_private_ip, dnat_server_port))
+        assert _relation_data(
+            json_data=json_data,
+            type_name="directional_connection",
+            external_id_assert_fn=lambda v: proc_to_proc_id_match.findall(v))["outgoing"]["ip"] == fedora_private_ip
+        assert _relation_data(
+            json_data=json_data,
+            type_name="directional_connection",
+            external_id_assert_fn=lambda v: proc_to_service_id_match.findall(v))["outgoing"]["ip"] == fedora_private_ip
+        assert _relation_data(
+            json_data=json_data,
+            type_name="directional_connection",
+            external_id_assert_fn=lambda v: service_to_proc_id_match.findall(v))["incoming"]["ip"] == ubuntu_private_ip
+
+    util.wait_until(wait_for_components, 30, 3)
