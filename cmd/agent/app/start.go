03aace8e5f615450dfe879b2f298f1c369757282
Vyacheslav Voronenko <git@voronenko.info>
2020-07-28 17:17:06 +0200

    chore(upstream changes) - recovering pipeline

--- cmd/agent/app/start.go
+++ cmd/agent/app/start.go
@@ -6,33 +6,7 @@
 package app
 
 import (
-	"github.com/StackVista/stackstate-agent/pkg/batcher"
 	"github.com/spf13/cobra"
-	"github.com/StackVista/stackstate-agent/cmd/agent/api"
-	"github.com/StackVista/stackstate-agent/cmd/agent/common"
-	"github.com/StackVista/stackstate-agent/cmd/agent/gui"
-	"github.com/StackVista/stackstate-agent/pkg/aggregator"
-	"github.com/StackVista/stackstate-agent/pkg/api/healthprobe"
-	"github.com/StackVista/stackstate-agent/pkg/collector/corechecks/embed/jmx"
-	"github.com/StackVista/stackstate-agent/pkg/config"
-	"github.com/StackVista/stackstate-agent/pkg/dogstatsd"
-	"github.com/StackVista/stackstate-agent/pkg/forwarder"
-	"github.com/StackVista/stackstate-agent/pkg/logs"
-	"github.com/StackVista/stackstate-agent/pkg/metadata"
-	"github.com/StackVista/stackstate-agent/pkg/metadata/host"
-	"github.com/StackVista/stackstate-agent/pkg/pidfile"
-	"github.com/StackVista/stackstate-agent/pkg/serializer"
-	"github.com/StackVista/stackstate-agent/pkg/status/health"
-	"github.com/StackVista/stackstate-agent/pkg/util"
-	"github.com/StackVista/stackstate-agent/pkg/util/log"
-	"github.com/StackVista/stackstate-agent/pkg/version"
-	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/cluster"
-	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/containers"
-	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/embed"
-	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/net"
-	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/system"
-	_ "github.com/StackVista/stackstate-agent/pkg/collector/metadata"
-	_ "github.com/StackVista/stackstate-agent/pkg/metadata"
 )
 
 var (
