a5eb2fedb3827c79b05a72f508c827cfa44095b8
Sylvain Baubeau <lebauce@gmail.com>
2020-08-06 23:07:16 +0200

    [runtime-security] Add functional tests for default policy (#6143)
    * Always setup logger in runtime security tests
    
    * Add task to run security agent functional tests in Docker
    
    * Fix 'permissions_changed' runtime agent rule
    
    * Fix runtime agent tests in non verbose mode
    
    * Add functional tests for runtime security default policy

--- pkg/security/tests/mitre_test.go
+++ pkg/security/tests/mitre_test.go
@@ -3,7 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-2020 Datadog, Inc.
 
-//go:build functionaltests
 // +build functionaltests
 
 package tests
@@ -15,9 +14,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/StackVista/stackstate-agent/pkg/security/config"
-	"github.com/StackVista/stackstate-agent/pkg/security/policy"
-	sprobe "github.com/StackVista/stackstate-agent/pkg/security/probe"
+	"github.com/DataDog/datadog-agent/pkg/security/config"
+	"github.com/DataDog/datadog-agent/pkg/security/policy"
+	sprobe "github.com/DataDog/datadog-agent/pkg/security/probe"
 )
 
 type testCase struct {
