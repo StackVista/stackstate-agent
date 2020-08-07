116d695321fcbe9187e5ae07ecbec3318ffd1e9d
Vyacheslav Voronenko <git@voronenko.info>
2020-08-07 14:48:18 +0200

    issues with openshift support

--- pkg/collector/corechecks/cluster/kubeapi/kubernetes_openshift.go
+++ pkg/collector/corechecks/cluster/kubeapi/kubernetes_openshift.go
@@ -3,8 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2017 Datadog, Inc.
 
-// TODO: [sts]
-// +build kubeapiserver_needsfix
+// +build kubeapiserver
 
 package kubeapi
 
