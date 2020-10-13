4dd777c3eee1497bc3c976495a92333bb272161a
zandrewitte <zandre.witte@gmail.com>
2020-10-13 16:27:53 +0200

    agent integration sample tests

--- test/molecule-role/molecule/integrations/tests/test_nagios.py
+++ test/molecule-role/molecule/integrations/tests/test_nagios.py
@@ -60,3 +60,29 @@ def test_nagios_mysql(host):
             ) is not None
 
     util.wait_until(assert_topology, 30, 3)
+
+
+def test_container_metrics(host):
+    url = "http://localhost:7070/api/topic/sts_multi_metrics?limit=1000"
+
+    def wait_for_metrics():
+        data = host.check_output("curl \"%s\"" % url)
+        json_data = json.loads(data)
+        with open("./topic-nagios-sts-multi-metrics.json", 'w') as f:
+            json.dump(json_data, f, indent=4)
+
+        def get_keys(m_host):
+            return set(
+                ''.join(message["message"]["MultiMetric"]["values"].keys())
+                for message in json_data["messages"]
+                if message["message"]["MultiMetric"]["name"] == "convertedMetric" and
+                message["message"]["MultiMetric"]["host"] == m_host
+            )
+
+        expected = {'nagios.http.size', 'nagios.ping.pl', 'nagios.http.time', 'nagios.current_load.load15',
+                    'nagios.swap_usage.swap', 'nagios.host.pl', 'nagios.root_partition', 'nagios.current_users.users',
+                    'nagios.current_load.load1', 'nagios.host.rta', 'nagios.ping.rta', 'nagios.current_load.load5',
+                    'nagios.total_processes.procs'}
+        assert all([expectedMetric for expectedMetric in expected if expectedMetric in get_keys("agent-integrations-mysql")])
+
+    util.wait_until(wait_for_metrics, 180, 3)
