145a18a5c352065d624916535a1f52f14f309b27
Pedro Lambert <pedro.lambert@datadoghq.com>
2020-10-22 17:00:51 -0400

    Add library for system-probe module heartbeats (#6525)

--- pkg/forwarder/forwarder.go
+++ pkg/forwarder/forwarder.go
@@ -200,6 +200,7 @@ type Options struct {
 	APIKeyValidationInterval       time.Duration
 	KeysPerDomain                  map[string][]string
 	ConnectionResetInterval        time.Duration
+	CompletionHandler              HTTPCompletionHandler
 }
 
 // NewOptions creates new Options with default values
@@ -244,6 +245,8 @@ type DefaultForwarder struct {
 	healthChecker    *forwarderHealth
 	internalState    uint32
 	m                sync.Mutex // To control Start/Stop races
+
+	completionHandler HTTPCompletionHandler
 }
 
 // NewDefaultForwarder returns a new DefaultForwarder.
@@ -258,6 +261,7 @@ func NewDefaultForwarder(options *Options) *DefaultForwarder {
 			disableAPIKeyChecking: options.DisableAPIKeyChecking,
 			validationInterval:    options.APIKeyValidationInterval,
 		},
+		completionHandler: options.CompletionHandler,
 	}
 
 	for domain, keys := range options.KeysPerDomain {
@@ -380,6 +384,10 @@ func (f *DefaultForwarder) createPriorityHTTPTransactions(endpoint endpoint, pay
 				t.Headers.Set(apiHTTPHeaderKey, apiKey)
 				t.Headers.Set(versionHTTPHeaderKey, version.AgentVersion)
 				t.Headers.Set(useragentHTTPHeaderKey, fmt.Sprintf("datadog-agent/%s", version.AgentVersion))
+
+				if f.completionHandler != nil {
+					t.completionHandler = f.completionHandler
+				}
 
 				tlm.Inc(domain, endpoint.name)
 
