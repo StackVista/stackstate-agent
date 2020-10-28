28e5cd7ba8a4ae5314062b67e2437f44f1b7a466
Vincent Boulineau <58430298+vboulineau@users.noreply.github.com>
2020-10-28 16:43:53 +0100

    Simplify and isolate Kubelet client (#6584)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -429,13 +429,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("kubelet_fallback_to_unverified_tls", true) // sts
 	config.BindEnvAndSetDefault("kubelet_fallback_to_insecure", true)       // sts
 	config.BindEnvAndSetDefault("collect_kubernetes_events", false)
-	config.BindEnvAndSetDefault("collect_kubernetes_metrics", false)
-	config.BindEnvAndSetDefault("collect_kubernetes_topology", false) // sts
-	config.BindEnvAndSetDefault("collect_kubernetes_timeout", 10)
-	config.BindEnvAndSetDefault("configmap_max_datasize", 0)                  // sts
-	config.BindEnvAndSetDefault("kubernetes_source_properties_enabled", true) // sts
-	config.BindEnvAndSetDefault("kubernetes_csi_pv_mapper_enabled", false)    // sts
-	config.BindEnvAndSetDefault("kubelet_client_ca", "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+	config.BindEnvAndSetDefault("kubelet_client_ca", "")
 
 	config.BindEnvAndSetDefault("kubelet_auth_token_path", "")
 	config.BindEnvAndSetDefault("kubelet_client_crt", "")
