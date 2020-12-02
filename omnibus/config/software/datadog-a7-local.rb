641179b62bf4967cbda75b166ca31f2e68e4fd58
Vyacheslav Voronenko <git@voronenko.info>
2020-12-02 14:59:08 +0100

    chore(experiment) is wheel pinnable for datadog-a7 qmark

--- omnibus/config/software/datadog-a7-local.rb
+++ omnibus/config/software/datadog-a7-local.rb
@@ -8,9 +8,11 @@ build do
   ship_license "https://raw.githubusercontent.com/DataDog/datadog-checks-shared/master/LICENSE"
   pip "install --install-option=\"--install-scripts="\
       "#{windows_safe_path(install_dir)}/bin\" "\
-      "#{name}==#{version} "\
-      "configparser==3.5.0 " \
       "wheel==0.30.0 "
+  pip "install --install-option=\"--install-scripts="\
+      "#{windows_safe_path(install_dir)}/bin\" "\
+      "wheel==0.30.0 "\
+      "#{name}==#{version} "\
+      "configparser==3.5.0 " # configparser==3.5.0 pins a dependency of pylint->datadog-a7, later versions (up to v3.7.1) are broken.
   # TODO: all deps should be pinned.
-  # # configparser==3.5.0 pins a dependency of pylint->datadog-a7, later versions (up to v3.7.1) are broken.
 end
