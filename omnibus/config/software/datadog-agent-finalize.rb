e32924d9af2db9042753730aae4eadf102213c1e
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-01-13 12:25:37 +0100

    [gitlab] Update rpm devtoolset in rpm builders (#7155)
    Updates the builders to include https://github.com/DataDog/datadog-agent-buildimages/pull/107.
    Updates the extended toolchain location in the `datadog-agent-finalize` definition.

--- omnibus/config/software/datadog-agent-finalize.rb
+++ omnibus/config/software/datadog-agent-finalize.rb
@@ -88,8 +88,7 @@ build do
             # Fix pip after building on extended toolchain in CentOS builder
             if redhat?
               unless arm?
-                # sts path is /opt/centos/devtoolset-1.1/root for centos6 based runner
-                rhel_toolchain_root = "/opt/rh/devtoolset-7/root"
+                rhel_toolchain_root = "/opt/rh/devtoolset-1.1/root"
                 # lets be cautious - we first search for the expected toolchain path, if its not there, bail out
                 command "find #{install_dir} -type f -iname '*_sysconfigdata*.py' -exec grep -inH '#{rhel_toolchain_root}' {} \\; |  egrep '.*'"
                 # replace paths with expected target toolchain location
