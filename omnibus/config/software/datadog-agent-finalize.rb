eeb6314cafbaa00dcccddbc42b099c4a488e1e88
Pedro Lambert <pedro.lambert@datadoghq.com>
2021-04-15 11:18:16 -0400

    [networks] Separate HTTP eBPF code from tracer code (#7886)

--- omnibus/config/software/datadog-agent-finalize.rb
+++ omnibus/config/software/datadog-agent-finalize.rb
@@ -204,6 +204,7 @@ build do
             # Do not strip eBPF programs
             strip_exclude("*tracer*")
             strip_exclude("*offset-guess*")
+            strip_exclude("*http*")
             strip_exclude("*runtime-security*")
         end
 
