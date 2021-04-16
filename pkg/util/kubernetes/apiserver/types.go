cb5d1c5c3e9594b1d56cdadbdb2a0d50827d82e0
Xavier Lucas <xavier.lucas@datadoghq.com>
2021-04-16 19:16:20 +0200

    Collect jobs and cronjobs (#7898)

--- pkg/util/kubernetes/apiserver/types.go
+++ pkg/util/kubernetes/apiserver/types.go
@@ -41,4 +41,8 @@ const (
 	ServicesInformer InformerName = "services"
 	// NodesInformer holds the name of the informer
 	NodesInformer InformerName = "nodes"
+	// JobsInformer holds the name of the informer
+	JobsInformer InformerName = "jobs"
+	// CronJobsInformer holds the name of the informer
+	CronJobsInformer InformerName = "cronJobs"
 )
