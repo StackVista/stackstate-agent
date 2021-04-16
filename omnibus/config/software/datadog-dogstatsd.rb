6b0fad98850cab75e6a4a3bb955fa4512a9de252
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-04-16 12:44:16 +0200

    [omnibus] Make go mod cache configurable (#7907)
    Makes the GOMODCACHE used in omnibus builds configurable, instead of a static location.
    Fixes the Windows build scripts so that the GOMODCACHE used in inv deps and omnibus commands is the same.

--- omnibus/config/software/datadog-dogstatsd.rb
+++ omnibus/config/software/datadog-dogstatsd.rb
@@ -16,12 +16,15 @@ relative_path 'src/github.com/StackVista/stackstate-agent'
 build do
   # set GOPATH on the omnibus source dir for this software
   gopath = Pathname.new(project_dir) + '../../../..'
-  gomodcache = Pathname.new("/gomodcache")
   env = {
     'GOPATH' => gopath.to_path,
-    'GOMODCACHE' => gomodcache.to_path,
     'PATH' => "#{gopath.to_path}/bin:#{ENV['PATH']}",
   }
+
+  unless ENV["OMNIBUS_GOMODCACHE"].nil? || ENV["OMNIBUS_GOMODCACHE"].empty?
+    gomodcache = Pathname.new(ENV["OMNIBUS_GOMODCACHE"])
+    env["GOMODCACHE"] = gomodcache.to_path
+  end
 
   if windows?
     major_version_arg = "%MAJOR_VERSION%"
