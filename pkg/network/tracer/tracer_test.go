4beee58a98064c16e3b51142f5e62936907f05aa
Hasan Mahmood <hmahmood@users.noreply.github.com>
2021-04-27 15:35:35 -0500

    Use correct function to check for cloud provider (#7984)

--- pkg/network/tracer/tracer_test.go
+++ pkg/network/tracer/tracer_test.go
@@ -2192,12 +2192,39 @@ func TestGatewayLookupNotEnabled(t *testing.T) {
 	})
 
 	t.Run("gateway lookup enabled, not on aws", func(t *testing.T) {
-		clouds := ddconfig.Datadog.Get("cloud_provider_metadata")
-		ddconfig.Datadog.Set("cloud_provider_metadata", []string{"gcp"})
-		defer ddconfig.Datadog.Set("cloud_provider_metadata", clouds)
-
 		cfg := testConfig()
 		cfg.EnableGatewayLookup = true
+		oldCloud := cloud
+		defer func() {
+			cloud = oldCloud
+		}()
+		ctrl := gomock.NewController(t)
+		m := NewMockcloudProvider(ctrl)
+		m.EXPECT().IsAWS().Return(false)
+		cloud = m
+		tr, err := NewTracer(cfg)
+		require.NoError(t, err)
+		require.NotNil(t, tr)
+		defer tr.Stop()
+		require.Nil(t, tr.gwLookup)
+	})
+
+	t.Run("gateway lookup enabled, aws metadata endpoint not enabled", func(t *testing.T) {
+		cfg := testConfig()
+		cfg.EnableGatewayLookup = true
+		oldCloud := cloud
+		defer func() {
+			cloud = oldCloud
+		}()
+		ctrl := gomock.NewController(t)
+		m := NewMockcloudProvider(ctrl)
+		m.EXPECT().IsAWS().Return(true)
+		cloud = m
+
+		clouds := ddconfig.Datadog.Get("cloud_provider_metadata")
+		ddconfig.Datadog.Set("cloud_provider_metadata", []string{})
+		defer ddconfig.Datadog.Set("cloud_provider_metadata", clouds)
+
 		tr, err := NewTracer(cfg)
 		require.NoError(t, err)
 		require.NotNil(t, tr)
@@ -2230,6 +2257,16 @@ func ipRouteGet(t *testing.T, from, dest string, iif *net.Interface) *net.Interf
 }
 
 func TestGatewayLookupEnabled(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	m := NewMockcloudProvider(ctrl)
+	oldCloud := cloud
+	defer func() {
+		cloud = oldCloud
+	}()
+
+	m.EXPECT().IsAWS().Return(true)
+	cloud = m
+
 	cfg := testConfig()
 	cfg.BPFDebug = true
 	cfg.EnableGatewayLookup = true
@@ -2269,6 +2306,16 @@ func TestGatewayLookupEnabled(t *testing.T) {
 }
 
 func TestGatewayLookupCrossNamespace(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	m := NewMockcloudProvider(ctrl)
+	oldCloud := cloud
+	defer func() {
+		cloud = oldCloud
+	}()
+
+	m.EXPECT().IsAWS().Return(true)
+	cloud = m
+
 	cfg := testConfig()
 	cfg.BPFDebug = true
 	cfg.EnableGatewayLookup = true
