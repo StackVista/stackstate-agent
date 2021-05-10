187de6aa1fc2978a40965c9ff37d204c87f0788e
Vickenty Fesunov <vickenty@users.noreply.github.com>
2021-05-10 11:03:29 +0200

    Vickenty/azure hostname (#8051)
    * [azure] get hostname from instance metadata
    
    Add a configuration setting to set hostname from the Azure instance
    metadata. Several options are provided, that generate host names with
    varying length and uniqueness.
    
    * add changelog entry
    
    * add default value to param line

--- pkg/util/hostname/azure.go
+++ pkg/util/hostname/azure.go
@@ -1,12 +1,7 @@
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License Version 2.0.
-
 package hostname
 
-import (
-	"github.com/StackVista/stackstate-agent/pkg/util/azure"
-)
+import "github.com/DataDog/datadog-agent/pkg/util/azure"
 
 func init() {
-	RegisterHostnameProvider("azure", azure.HostnameProvider)
+	RegisterHostnameProvider("azure", azure.GetHostname)
 }
