6f2e61e1a4891d64739f1bf9af81adee6e0386c9
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2021-05-17 18:11:22 +0200

    Replace golint with revive (#8160)
    -Use revive instead of golint, since golint was deprecated.
    - Fix issues found by revive.

--- pkg/collector/corechecks/containers/containerd/containerd.go
+++ pkg/collector/corechecks/containers/containerd/containerd.go
@@ -74,12 +74,7 @@ func ContainerdFactory() check.Check {
 
 // Parse is used to get the configuration set by the user
 func (co *ContainerdConfig) Parse(data []byte) error {
-	// sts
-	co.CollectContainerTopology = true
-	if err := yaml.Unmarshal(data, co); err != nil {
-		return err
-	}
-	return nil
+	return yaml.Unmarshal(data, co)
 }
 
 // Configure parses the check configuration and init the check
--- pkg/collector/corechecks/containers/cri/cri.go
+++ pkg/collector/corechecks/containers/cri/cri.go
@@ -67,10 +67,7 @@ func (c *CRIConfig) Parse(data []byte) error {
 	// sts
 	c.CollectContainerTopology = true
 
-	if err := yaml.Unmarshal(data, c); err != nil {
-		return err
-	}
-	return nil
+	return yaml.Unmarshal(data, c)
 }
 
 // Configure parses the check configuration and init the check
