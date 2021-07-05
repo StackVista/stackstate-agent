b7f1891e9bb00b36c7b25d07940c24434e081272
Pierre Margueritte <pierre.margueritte@datadoghq.com>
2021-07-05 10:55:00 +0200

    Increase default timeout to sync informers from 2 to 5 seconds (#8542)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -557,7 +557,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("leader_lease_duration", "60")
 	config.BindEnvAndSetDefault("leader_election", false)
 	config.BindEnvAndSetDefault("kube_resources_namespace", "")
-	config.BindEnvAndSetDefault("kube_cache_sync_timeout_seconds", 2)
+	config.BindEnvAndSetDefault("kube_cache_sync_timeout_seconds", 5)
 
 	// Datadog cluster agent
 	config.BindEnvAndSetDefault("cluster_agent.enabled", false)
