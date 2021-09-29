afb4740f2e35960245fe96946997b779aa9caaa3
Melcom van Eeden <melcomve@outlook.com>
2021-09-29 17:39:45 +0200

    Removed deps deb

--- .assistance/gitlab/deps_deb.sh
+++
@@ -1,6 +0,0 @@
-conda activate $CONDA_ENV
-inv -e deps --verbose --dep-vendor-only
-inv agent.version --major-version $MAJOR_VERSION -u > version.txt
-cd $GOPATH/pkg && tar czf $CI_PROJECT_DIR/go-pkg.tar.gz .
-cd $GOPATH/bin && tar czf $CI_PROJECT_DIR/go-bin.tar.gz .
-cd $CI_PROJECT_DIR/vendor && tar czf $CI_PROJECT_DIR/vendor.tar.gz .
