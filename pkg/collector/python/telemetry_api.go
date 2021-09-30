f6f39d0cc7502e69087482e13c001a711f048795
Melcom van Eeden <melcomve@outlook.com>
2021-09-30 02:11:30 +0200

    Test submit - checkid

--- pkg/collector/python/telemetry_api.go
+++ pkg/collector/python/telemetry_api.go
@@ -59,14 +59,15 @@ func SubmitTopologyEvent(id *C.char, data *C.char) {
 // SubmitRawMetricsData
 //export SubmitRawMetricsData
 func SubmitRawMetricsData(id *C.char, name *C.char, value C.float, tags **C.char, hostname *C.char, timestamp C.longlong) {
-	// checkID := C.GoString(id)
+	checkID := C.GoString(id)
+
 	// rawName := C.GoString(name)
 	// rawHostname := C.GoString(hostname)
 	// rawValue := float64(value)
 	// rawTimestamp := int64(timestamp)
 	// rawTags := cStringArrayToSlice(tags)
 
-	batcher.GetBatcher().SubmitRawMetricsData(check.ID("random-id"), telemetry.RawMetricsCheckData{
+	batcher.GetBatcher().SubmitRawMetricsData(checkID, telemetry.RawMetricsCheckData{
 		Name:      "name",
 		Timestamp: 140000,
 		HostName:  "hostname",
