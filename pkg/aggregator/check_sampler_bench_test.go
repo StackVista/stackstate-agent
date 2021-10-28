8ff2d6093848e87fc34c583ddea55a6b5cffe5be
Pierre Rognant <59561183+prognant@users.noreply.github.com>
2021-10-28 09:58:22 +0200

    [config+forwarder] Add the ability to divert metrics to Vector (#9633)
    Add a configuration key to divert metrics request to an alternate
    endpoint. Include some work to easily extend the kind of data
    that can be sent to Vector.

--- pkg/aggregator/check_sampler_bench_test.go
+++ pkg/aggregator/check_sampler_bench_test.go
@@ -8,10 +8,11 @@ package aggregator
 import (
 	"testing"
 
-	"github.com/StackVista/stackstate-agent/pkg/aggregator/ckey"
-	"github.com/StackVista/stackstate-agent/pkg/forwarder"
-	"github.com/StackVista/stackstate-agent/pkg/metrics"
-	"github.com/StackVista/stackstate-agent/pkg/serializer"
+	"github.com/DataDog/datadog-agent/pkg/aggregator/ckey"
+	"github.com/DataDog/datadog-agent/pkg/config/resolver"
+	"github.com/DataDog/datadog-agent/pkg/forwarder"
+	"github.com/DataDog/datadog-agent/pkg/metrics"
+	"github.com/DataDog/datadog-agent/pkg/serializer"
 )
 
 func benchmarkAddBucket(bucketValue int64, b *testing.B) {
@@ -20,7 +21,7 @@ func benchmarkAddBucket(bucketValue int64, b *testing.B) {
 	// For some reasons using InitAggregator[WithInterval] doesn't fix the problem,
 	// but this do.
 	aggregatorInstance.serializer = serializer.NewSerializer(forwarder.NewDefaultForwarder(
-		forwarder.NewOptions(map[string][]string{"hello": {"world"}})),
+		forwarder.NewOptionsWithResolvers(resolver.NewSingleDomainResolvers(map[string][]string{"hello": {"world"}}))),
 		nil,
 	)
 	checkSampler := newCheckSampler(1, true, 1000)
