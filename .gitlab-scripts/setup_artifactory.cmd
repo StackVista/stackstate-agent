de58da380e389d0b7cf19ae64226f0521b39ac5e
Juliano Krieger <julianosk@gmail.com>
2021-12-08 13:41:25 +0100

    Remove /simple from pypi url

--- .gitlab-scripts/setup_artifactory.cmd
+++ .gitlab-scripts/setup_artifactory.cmd
@@ -1,4 +1,4 @@
 if exist %APPDATA%\pip rd /s/q %APPDATA%\pip
 mkdir %APPDATA%\pip
 echo [global] > %APPDATA%\pip\pip.ini
-echo extra-index-url = https://%artifactory_user%:%artifactory_password%@%ARTIFACTORY_PYPI_URL%/simple >> %APPDATA%\pip\pip.ini
+echo extra-index-url = https://%artifactory_user%:%artifactory_password%@%ARTIFACTORY_PYPI_URL% >> %APPDATA%\pip\pip.ini
