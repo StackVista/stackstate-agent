0c64dd7282d3c7f31e1ee2350599c3680d7cbc32
Alexander Hasselbach <agasselbakh@stackstate.com>
2022-02-17 18:59:11 +0300

    STAC-15054 fix format

--- pkg/collector/corechecks/cluster/topologycollectors/common_test.go
+++ pkg/collector/corechecks/cluster/topologycollectors/common_test.go
@@ -5,18 +5,17 @@ import "github.com/StackVista/stackstate-agent/pkg/topology"
 func testCaseName(baseName string, sourcePropertiesEnabled bool) string {
 	if sourcePropertiesEnabled {
 		return baseName + " with sourceProperties"
-	} else {
-		return baseName + " without sourceProperties"
 	}
+	return baseName + " without sourceProperties"
 }
 
 func chooseBySourcePropertiesFeature(
 	sourcePropertiesEnabled bool,
 	componentNoSP *topology.Component,
-	componentSP *topology.Component) *topology.Component {
+	componentSP *topology.Component,
+) *topology.Component {
 	if sourcePropertiesEnabled {
 		return componentSP
-	} else {
-		return componentNoSP
 	}
+	return componentNoSP
 }
