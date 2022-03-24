258083b5889c4cd5f290ac8c4b7afa71fb9c50dc
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2022-03-24 09:56:47 +0100

    Added missing goto errors

--- rtloader/common/builtins/topology.c
+++ rtloader/common/builtins/topology.c
@@ -158,6 +158,9 @@ done:
     Py_XDECREF(component);
     PyGILState_Release(gstate);
     return retval;
+error:
+    PyGILState_Release(gstate);
+    return NULL; // Failure
 }
 
 /*! \fn submit_relation(PyObject *self, PyObject *args)
@@ -246,6 +249,9 @@ done:
     Py_XDECREF(relation);
     PyGILState_Release(gstate);
     return retval;
+error:
+    PyGILState_Release(gstate);
+    return NULL; // Failure
 }
 
 /*! \fn submit_start_snapshot(PyObject *self, PyObject *args)
