a9caadd7e9bb37d2940d555aa64c4a4c43c26924
Zandré Witte <zwitte@stackstate.com>
2022-04-11 12:00:54 +0200

    transaction manager improvements, tests WIP

--- pkg/collector/transactional/manager/transaction_manager.go
+++ pkg/collector/transactional/manager/transaction_manager.go
@@ -1,6 +1,7 @@
 package manager
 
 import (
+	"fmt"
 	"github.com/StackVista/stackstate-agent/pkg/collector/check"
 	"github.com/StackVista/stackstate-agent/pkg/util/log"
 	"time"
@@ -29,14 +30,45 @@ type StartTransaction struct {
 	OnComplete    func(transaction *IntakeTransaction)
 }
 
+// CompleteTransaction ...
+type CompleteTransaction struct {
+	TransactionID string
+}
+
 // RollbackTransaction ...
 type RollbackTransaction struct {
 	TransactionID string
-	Reason        error
+	Reason        string
+}
+
+func (r RollbackTransaction) Error() string {
+	return fmt.Sprintf("rolling back transaction %s. %s", r.TransactionID, r.Reason)
 }
 
 // StopTransactionManager ...
 type StopTransactionManager struct{}
+
+type TransactionManagerNotRunning struct{}
+
+func (t TransactionManagerNotRunning) Error() string {
+	return "transaction manager is not running, call TransactionManager.Start() to start it"
+}
+
+type TransactionNotFound struct {
+	TransactionID string
+}
+
+func (t TransactionNotFound) Error() string {
+	return fmt.Sprintf("transaction %s not found in transaction manager", t.TransactionID)
+}
+
+type ActionNotFound struct {
+	TransactionID, ActionID string
+}
+
+func (a ActionNotFound) Error() string {
+	return fmt.Sprintf("action %s for transaction %s not found in transaction manager", a.ActionID, a.TransactionID)
+}
 
 // TransactionManager ...
 type TransactionManager struct {
@@ -45,6 +77,7 @@ type TransactionManager struct {
 	Transactions                map[string]*IntakeTransaction
 	transactionTimeoutDuration  time.Duration
 	transactionEvictionDuration time.Duration
+	running                     bool
 }
 
 // MakeTransactionManager ...
@@ -67,21 +100,43 @@ func (txm *TransactionManager) Start() {
 			select {
 			case input := <-txm.TransactionChannel:
 				switch msg := input.(type) {
+				// transaction operations
 				case StartTransaction:
 					log.Debugf("Creating new transaction %s for check %s", msg.TransactionID, msg.CheckID)
-					txm.startTransaction(msg.TransactionID, msg.OnComplete)
+					if _, err := txm.startTransaction(msg.TransactionID, msg.OnComplete); err != nil {
+						txm.TransactionChannel <- err
+					}
 				case CommitAction:
-					log.Debugf("Committing msg %s for manager %s", msg.ActionID, msg.TransactionID)
-					txm.commitAction(msg.TransactionID, msg.ActionID)
+					log.Debugf("Committing action %s for transaction %s", msg.ActionID, msg.TransactionID)
+					if err := txm.commitAction(msg.TransactionID, msg.ActionID); err != nil {
+						txm.TransactionChannel <- err
+					}
 				case AckAction:
-					log.Debugf("Acknowledging msg %s for manager %s", msg.ActionID, msg.TransactionID)
-					txm.ackAction(msg.TransactionID, msg.ActionID)
+					log.Debugf("Acknowledging action %s for transaction %s", msg.ActionID, msg.TransactionID)
+					if err := txm.ackAction(msg.TransactionID, msg.ActionID); err != nil {
+						txm.TransactionChannel <- err
+					}
 				case RejectAction:
-					_ = log.Errorf("Rejecting msg %s for manager %s: %s", msg.ActionID, msg.TransactionID, msg.Reason)
-					txm.rejectAction(msg.TransactionID, msg.ActionID)
+					_ = log.Errorf("Rejecting action %s for transaction %s: %s", msg.ActionID, msg.TransactionID, msg.Reason)
+					if err := txm.rejectAction(msg.TransactionID, msg.ActionID); err != nil {
+						txm.TransactionChannel <- err
+					}
+				case CompleteTransaction:
+					log.Debugf("Completing transaction %s", msg.TransactionID)
+					if err := txm.completeTransaction(msg.TransactionID); err != nil {
+						txm.TransactionChannel <- err
+					}
+				// error cases
 				case RollbackTransaction:
-					_ = log.Errorf("Rolling back manager %s: %s", msg.TransactionID, msg.Reason)
-					txm.rollbackTransaction(msg.TransactionID)
+					_ = log.Errorf(msg.Error())
+					if err := txm.rollbackTransaction(msg.TransactionID); err != nil {
+						txm.TransactionChannel <- err
+					}
+				case TransactionNotFound:
+					_ = log.Errorf(msg.Error())
+				case ActionNotFound:
+					txm.TransactionChannel <- RollbackTransaction{TransactionID: msg.TransactionID, Reason: msg.Error()}
+				// shutdown transaction manager
 				case StopTransactionManager:
 					break transactionHandler
 				default:
@@ -101,20 +156,29 @@ func (txm *TransactionManager) Start() {
 						txm.evictTransaction(transaction.TransactionID)
 					}
 				}
+
+				// TODO: produce some transaction manager metrics
 			default:
 			}
 		}
 	}()
+
+	txm.running = true
 }
 
 // Stop ...
 func (txm *TransactionManager) Stop() {
+	txm.running = false
 	txm.TransactionChannel <- StopTransactionManager{}
 	txm.TransactionTicker.Stop()
 }
 
 // startTransaction ...
-func (txm *TransactionManager) startTransaction(transactionID string, onComplete func(transaction *IntakeTransaction)) *IntakeTransaction {
+func (txm *TransactionManager) startTransaction(transactionID string, onComplete func(transaction *IntakeTransaction)) (*IntakeTransaction, error) {
+	if !txm.running {
+		return nil, TransactionManagerNotRunning{}
+	}
+
 	transaction := &IntakeTransaction{
 		TransactionID:        transactionID,
 		State:                InProgress,
@@ -125,78 +189,93 @@ func (txm *TransactionManager) startTransaction(transactionID string, onComplete
 
 	txm.Transactions[transaction.TransactionID] = transaction
 
-	return transaction
+	return transaction, nil
 }
 
 // commitAction ...
-func (txm *TransactionManager) commitAction(transactionID, actionID string) {
+func (txm *TransactionManager) commitAction(transactionID, actionID string) error {
 	transaction, exists := txm.Transactions[transactionID]
-	if exists {
-		action := &Action{
-			ActionID:           actionID,
-			CommittedTimestamp: time.Now(),
-		}
-		txm.updateAction(transaction, action, InProgress)
-		log.Debugf("Transaction %s, committing action %s", transactionID, actionID)
+	if !exists {
+		return TransactionNotFound{TransactionID: transactionID}
 	}
+
+	action := &Action{
+		ActionID:           actionID,
+		CommittedTimestamp: time.Now(),
+	}
+	txm.updateTransaction(transaction, action, InProgress)
+	log.Debugf("Transaction %s, committing action %s", transactionID, actionID)
+
+	return nil
 }
 
-func (txm *TransactionManager) updateAction(transaction *IntakeTransaction, action *Action, state TransactionState) {
+func (txm *TransactionManager) updateTransaction(transaction *IntakeTransaction, action *Action, state TransactionState) {
 	transaction.Actions[action.ActionID] = action
 	transaction.State = state
 	transaction.LastUpdatedTimestamp = time.Now()
 }
 
 // ackAction ...
-func (txm *TransactionManager) ackAction(transactionID, actionID string) {
+func (txm *TransactionManager) ackAction(transactionID, actionID string) error {
 	transaction, exists := txm.Transactions[transactionID]
-	if exists {
-		action, exists := transaction.Actions[actionID]
-		if exists {
-			action.Acknowledged = true
-			action.AcknowledgedTimestamp = time.Now()
-			txm.updateAction(transaction, action, InProgress)
-			log.Debugf("Transaction %s, acknowledged action %s", transactionID, actionID)
-		}
+	if !exists {
+		return TransactionNotFound{TransactionID: transactionID}
 	}
+
+	action, exists := transaction.Actions[actionID]
+	if !exists {
+		return ActionNotFound{ActionID: actionID, TransactionID: transactionID}
+	}
+	action.Acknowledged = true
+	action.AcknowledgedTimestamp = time.Now()
+	txm.updateTransaction(transaction, action, InProgress)
+	log.Debugf("Transaction %s, acknowledged action %s", transactionID, actionID)
+
+	return nil
 }
 
 // rejectAction ...
-func (txm *TransactionManager) rejectAction(transactionID, actionID string) {
+func (txm *TransactionManager) rejectAction(transactionID, actionID string) error {
 	transaction, exists := txm.Transactions[transactionID]
-	if exists {
-		action, exists := transaction.Actions[actionID]
-		if exists {
-			action.Acknowledged = true
-			action.AcknowledgedTimestamp = time.Now()
-			txm.updateAction(transaction, action, Failed)
-			log.Debugf("Transaction %s, acknowledged action %s", transactionID, actionID)
-			txm.rollbackTransaction(transactionID)
-		}
+	if !exists {
+		return TransactionNotFound{TransactionID: transactionID}
 	}
+
+	action, exists := transaction.Actions[actionID]
+	if !exists {
+		return ActionNotFound{ActionID: actionID, TransactionID: transactionID}
+	}
+	action.Acknowledged = true
+	action.AcknowledgedTimestamp = time.Now()
+	txm.updateTransaction(transaction, action, Failed)
+	log.Debugf("Transaction %s, acknowledged action %s", transactionID, actionID)
+
+	return nil
 }
 
 // completeTransaction marks the manager successful
-func (txm *TransactionManager) completeTransaction(transactionID string) {
+func (txm *TransactionManager) completeTransaction(transactionID string) error {
 	transaction, exists := txm.Transactions[transactionID]
-	if exists {
-		// ensure all actions have been acknowledged
-		for _, action := range transaction.Actions {
-			if !action.Acknowledged {
-				_ = log.Errorf("Not all actions have been acknowledged, rolling back manager: %s", transaction.TransactionID)
-				txm.rollbackTransaction(transactionID)
-			}
-		}
-		transaction.State = Succeeded
-		transaction.LastUpdatedTimestamp = time.Now()
-		log.Debugf("Transaction succeeded %s", transaction.TransactionID)
-	} else {
-		_ = log.Warnf("Transaction not found %s, no operation", transaction.TransactionID)
+	if !exists {
+		return TransactionNotFound{TransactionID: transactionID}
 	}
+
+	// ensure all actions have been acknowledged
+	for _, action := range transaction.Actions {
+		if !action.Acknowledged {
+			reason := fmt.Sprintf("Not all actions have been acknowledged, rolling back manager: %s", transaction.TransactionID)
+			return RollbackTransaction{TransactionID: transactionID, Reason: reason}
+		}
+	}
+	transaction.State = Succeeded
+	transaction.LastUpdatedTimestamp = time.Now()
+	log.Debugf("Transaction succeeded %s", transaction.TransactionID)
 
 	if transaction.OnComplete != nil {
 		transaction.OnComplete(transaction)
 	}
+
+	return nil
 }
 
 // evictTransaction evicts the intake manager due to failure or timeout
@@ -205,16 +284,20 @@ func (txm *TransactionManager) evictTransaction(transactionID string) {
 }
 
 // rollbackTransaction rolls back the intake manager
-func (txm *TransactionManager) rollbackTransaction(transactionID string) {
+func (txm *TransactionManager) rollbackTransaction(transactionID string) error {
 	transaction, exists := txm.Transactions[transactionID]
-	if exists {
-		// manager failed, rollback
-		transaction.State = Failed
-		transaction.LastUpdatedTimestamp = time.Now()
-		log.Debugf("Transaction failed %s", transaction.TransactionID)
+	if !exists {
+		return TransactionNotFound{TransactionID: transactionID}
 	}
+
+	// manager failed, rollback
+	transaction.State = Failed
+	transaction.LastUpdatedTimestamp = time.Now()
+	log.Debugf("Transaction failed %s", transaction.TransactionID)
 
 	if transaction.OnComplete != nil {
 		transaction.OnComplete(transaction)
 	}
+
+	return nil
 }
--- pkg/collector/transactional/manager/transaction_manager_test.go
+++ pkg/collector/transactional/manager/transaction_manager_test.go
@@ -1,25 +1,28 @@
 package manager
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
-func TestMakeTransactionManager(t *testing.T) {
-	transactionManager := MakeTransactionManager(100, 100*time.Millisecond, 10*time.Second,
-		10*time.Second)
+func TestTransactionManager_HappyFlow(t *testing.T) {
+	transactionManager := MakeTransactionManager(100, 100*time.Millisecond, 500*time.Millisecond,
+		500*time.Millisecond)
 
+	assert.False(t, transactionManager.running)
 	transactionManager.Start()
+	assert.True(t, transactionManager.running)
 
 	// assert that we're starting on a clean slate
 	assert.Len(t, transactionManager.Transactions, 0)
 
 	completeFunc := func(transaction *IntakeTransaction) {
 		// assert that LastUpdatedTimestamp was set in the last 50 milliseconds. i.e. just updated
-		assert.Equal(t, time.Now().After(time.Now().Add(-50*time.Millisecond)), transaction.LastUpdatedTimestamp)
-		assert.Equal(t, InProgress, transaction.State)
+		assert.True(t, transaction.LastUpdatedTimestamp.After(time.Now().Add(-50*time.Millisecond)))
+		assert.Equal(t, Succeeded, transaction.State)
 	}
 
 	// start a transaction and assert it
@@ -27,18 +30,96 @@ func TestMakeTransactionManager(t *testing.T) {
 	transactionManager.TransactionChannel <- StartTransaction{TransactionID: txID, OnComplete: completeFunc}
 	assertTransaction(t, transactionManager, txID, InProgress, map[string]*Action{})
 
-	time.Sleep(500 * time.Millisecond)
+	// commit 15 actions and assert them
+	actions := make(map[string]*Action, 5)
+	for i := 0; i < 15; i++ {
+		actID := fmt.Sprintf("action-%d", i)
+		transactionManager.TransactionChannel <- CommitAction{TransactionID: txID, ActionID: actID}
+		actions[actID] = &Action{ActionID: actID, Acknowledged: false}
+		assertTransaction(t, transactionManager, txID, InProgress, actions)
+	}
+	// acknowledge 15 action and assert them
+	for i := 0; i < 15; i++ {
+		actID := fmt.Sprintf("action-%d", i)
+		transactionManager.TransactionChannel <- AckAction{TransactionID: txID, ActionID: actID}
+		actions[actID] = &Action{ActionID: actID, Acknowledged: true}
+		assertTransaction(t, transactionManager, txID, InProgress, actions)
+	}
+
+	// start a transaction and assert it
+	transactionManager.TransactionChannel <- CompleteTransaction{TransactionID: txID}
+	assertTransaction(t, transactionManager, txID, Succeeded, actions)
+
 	defer transactionManager.Stop()
 
+	// sleep and wait for automatic cleanup to remove the successful transaction
+	time.Sleep(1 * time.Second)
+	assert.Len(t, transactionManager.Transactions, 0)
+}
+
+func TestTransactionManager_TransactionRollback(t *testing.T) {
+	transactionManager := MakeTransactionManager(100, 100*time.Millisecond, 500*time.Millisecond,
+		500*time.Millisecond)
+
+	transactionManager.Start()
+
+	for _, tc := range []struct {
+		testCase  string
+		operation func(t *testing.T, manager *TransactionManager)
+	}{
+		{
+			testCase: "Transaction rollback triggered by external party (check handler)",
+			operation: func(t *testing.T, manager *TransactionManager) {
+				txID := uuid.New().String()
+				transactionManager.TransactionChannel <- StartTransaction{TransactionID: txID}
+				assertTransaction(t, transactionManager, txID, InProgress, map[string]*Action{})
+				transactionManager.TransactionChannel <- RollbackTransaction{TransactionID: txID, Reason: "check failed"}
+				assertTransaction(t, transactionManager, txID, Failed, map[string]*Action{})
+			},
+		},
+		{
+			testCase: "Transaction rollback triggered by an un-acknowledged action",
+			operation: func(t *testing.T, manager *TransactionManager) {
+				txID := uuid.New().String()
+				transactionManager.TransactionChannel <- StartTransaction{TransactionID: txID}
+				assertTransaction(t, transactionManager, txID, InProgress, map[string]*Action{})
+
+				actions := make(map[string]*Action, 1)
+				actID := uuid.New().String()
+				transactionManager.TransactionChannel <- CommitAction{TransactionID: txID, ActionID: actID}
+				actions[actID] = &Action{ActionID: actID, Acknowledged: false}
+				assertTransaction(t, transactionManager, txID, InProgress, actions)
+
+				transactionManager.TransactionChannel <- CompleteTransaction{TransactionID: txID}
+				assertTransaction(t, transactionManager, txID, Failed, actions)
+			},
+		},
+	} {
+		t.Run(tc.testCase, func(t *testing.T) {
+			tc.operation(t, transactionManager)
+		})
+	}
+
+	defer transactionManager.Stop()
+
+	// sleep and wait for automatic cleanup to remove the successful transaction
+	time.Sleep(1 * time.Second)
+	assert.Len(t, transactionManager.Transactions, 0)
 }
 
 func assertTransaction(t *testing.T, transactionManager *TransactionManager, txID string, state TransactionState,
 	actions map[string]*Action) {
-	time.Sleep(250 * time.Millisecond)
-	assert.Len(t, transactionManager.Transactions, 1)
+	// give the transaction manager a bit of time to insert the transaction before running the assertion
+	time.Sleep(10 * time.Millisecond)
 	transaction, found := transactionManager.Transactions[txID]
 	assert.True(t, found, "Transaction %s not found in the transaction map", txID)
 	assert.Equal(t, txID, transaction.TransactionID)
 	assert.Equal(t, state, transaction.State)
-	assert.EqualValues(t, actions, transaction.Actions)
+	assert.Equal(t, len(actions), len(transaction.Actions))
+	for _, action := range transaction.Actions {
+		expectedAction, found := actions[action.ActionID]
+		assert.True(t, found)
+		assert.Equal(t, expectedAction.ActionID, action.ActionID)
+		assert.Equal(t, expectedAction.Acknowledged, action.Acknowledged)
+	}
 }
