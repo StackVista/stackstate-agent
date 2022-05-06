d7a7ce8933ea4a68f7cede1add6338fb99cde9cb
Alexander Hasselbach <agasselbakh@stackstate.com>
2022-05-06 16:46:06 +0300

    Fallback for backward compatibility with python check

--- pkg/health/stream.go
+++ pkg/health/stream.go
@@ -9,6 +9,24 @@ type Stream struct {
 	SubStream string `json:"sub_stream_id,omitempty"`
 }
 
+func (i *Stream) UnmarshalJSON(buf []byte) error {
+	data := map[string]string{}
+	if err := json.Unmarshal(buf, &data); err != nil {
+		return err
+	}
+	urn, ok := data["urn"]
+	if !ok {
+		return fmt.Errorf("urn is missing")
+	}
+	i.Urn = urn
+	subStream, ok := data["sub_stream_id"]
+	if !ok {
+		subStream, _ = data["sub_stream"] // override if something is there
+	}
+	i.SubStream = subStream
+	return nil
+}
+
 // GoString prints as string, can also be used in maps
 func (i *Stream) GoString() string {
 	b, err := json.Marshal(i)
