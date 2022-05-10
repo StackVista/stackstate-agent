ab3d9174a3dc1b0c2605f3363e0e10868cc6a6ca
Johnny Gladwin <johnny.gladwin@outlook.com>
2022-05-10 16:52:01 +0530

    otel prepare

--- beest/sut/hives/lambda-otel/outputs.tf
+++ beest/sut/hives/lambda-otel/outputs.tf
@@ -1,3 +1,9 @@
 output "api_url" {
   value = aws_api_gateway_deployment.deployment.invoke_url
 }
+output "bucket" {
+  value = aws_s3_bucket.bucket.id
+}
+output "codepath" {
+  value = aws_s3_object.code_zip.source
+}
--- beest/sut/yards/otel-custom/outputs.tf
+++ beest/sut/yards/otel-custom/outputs.tf
@@ -1,3 +1,21 @@
 output "api_url" {
   value = module.otel.api_url
 }
+output "bucket" {
+  value = module.otel.bucket
+}
+
+resource "local_file" "ansible_inventory" {
+  filename        = "${path.module}/ansible_inventory"
+  content         = <<INVENTORY
+
+[local]
+localhost ansible_connection=local
+
+[all:vars]
+yard_id=${var.yard_id}
+bucket="${module.otel.bucket}"
+code_zip="${module.otel.codepath}"
+INVENTORY
+  file_permission = "0777"
+}
