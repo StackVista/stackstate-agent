2668247ad625a40caac6cfecd017e28f9440d3b4
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2022-05-13 16:21:46 +0200

    improve local shell init

--- Dockerfiles/local_builder/shell_init.sh
+++ Dockerfiles/local_builder/shell_init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 source /root/.bashrc
-conda activate ddpy3
-source ./.gitlab-scripts/setup_artifactory.sh
+conda activate ddpy2
+source /go/src/github.com/StackVista/stackstate-agent/.gitlab-scripts/setup_artifactory.sh
 
 # inv -e deps --verbose --dep-vendor-only
 #
-# inv rtloader.clean && inv rtloader.make --python-runtimes 3 && inv rtloader.test
+# inv rtloader.clean && inv rtloader.make --python-runtimes 2 && inv rtloader.test
+#
+# inv -e agent.omnibus-build --gem-path $CI_PROJECT_DIR/.gems --base-dir $OMNIBUS_BASE_DIR --skip-deps --skip-sign --major-version 2 --python-runtimes 2
+# inv -e agent.omnibus-build --skip-deps --skip-sign --major-version 2 --python-runtimes 2
