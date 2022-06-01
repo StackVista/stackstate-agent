0b98b88647d022b8bf41234fa97a968c8d6093a3
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2022-06-01 16:01:53 +0200

    beest: - install aws cli v2 to avoid boto3 conflicts and stop using deprecated cli - bump sts-toolbox to 1.3.29 - re-order bootstrap commands

--- beest/bootstrap.sh
+++ beest/bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+go install github.com/spf13/cobra/cobra
+complete -C '/usr/local/bin/aws_completer' aws
+eval "$(sts-toolbox completion bash)"
+
 source ~/.bashrc
 eval "$(direnv hook bash)"
 direnv allow
 
-go install github.com/spf13/cobra/cobra
-
 go build .
 eval "$(/go/src/app/beest completion bash)"
-
-eval "$(sts-toolbox completion bash)"
