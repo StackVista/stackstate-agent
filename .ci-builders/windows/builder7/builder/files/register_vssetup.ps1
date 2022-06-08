c91a96253c0d0f1087b297c62eda506bf5791e9c
Louis Parkin <lparkin@stackstate.com>
2022-06-08 16:50:03 +0200

    STAC-16454 Changes to packer build to address missing dependencies.

--- .ci-builders/windows/builder7/builder/files/register_vssetup.ps1
+++ .ci-builders/windows/builder7/builder/files/register_vssetup.ps1
@@ -3,7 +3,4 @@ $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module Pscx -AllowClobber
-Install-Module VSSetup -Scope CurrentUser
+Register-PSRepository -Default -Verbose
