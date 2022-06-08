5d595be3a29dcf0f12cc479325eda45cac1b270b
Melcom van Eeden <melcomve@outlook.com>
2022-06-08 19:11:16 +0200

    Fix incorrect setSet removal

--- rtloader/three/three.cpp
+++ rtloader/three/three.cpp
@@ -861,7 +861,7 @@ void Three::setLogCb(cb_log_t cb)
     _set_log_cb(cb);
 }
 
-void Three::setCheckMetadataCb(cb_set_check_metadata_t cb)
+void Three::setSetCheckMetadataCb(cb_set_check_metadata_t cb)
 {
     _set_set_check_metadata_cb(cb);
 }
