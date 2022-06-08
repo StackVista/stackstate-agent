9123ecff2ed58f46897a5d0e2e626d09489b1bcf
Juliano Krieger <julianosk@gmail.com>
2022-06-08 13:16:46 +0200

    Increased timeout since sts trace agent also interprets each span [3]
    Signed-off-by: Juliano Krieger <julianosk@gmail.com>

--- pkg/trace/agent/agent_test.go
+++ pkg/trace/agent/agent_test.go
@@ -283,7 +283,7 @@ func TestProcess(t *testing.T) {
 		}, stats.NewSublayerCalculator())
 
 		var gotCount int
-		timeout := time.After(5 * time.Second)
+		timeout := time.After(6 * time.Second)
 		// expect multiple payloads
 		for i := 0; i < payloadN+2; i++ {
 			select {
