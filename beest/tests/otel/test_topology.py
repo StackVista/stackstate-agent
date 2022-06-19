88d04d48a40de5d8aaeb718ebeb424121c546f17
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2022-06-19 12:34:32 +0200

    beest otel: - introduce wait_until

--- beest/tests/otel/test_topology.py
+++ beest/tests/otel/test_topology.py
@@ -1,8 +1,11 @@
+import util
 
 testinfra_hosts = ["local"]
 
 
-def test_something(cliv1):
-    json = cliv1("label IN ('stackpack:aws-v2')")
-    assert len(json["result"]) > 500
+def test_not_empty(cliv1):
+    def assert_it():
+        json = cliv1("label IN ('stackpack:aws-v2')")
+        assert len(json["result"]) > 500
 
+    util.wait_until(assert_it, 30, 5)
