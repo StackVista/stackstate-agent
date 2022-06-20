eb6325cff3cbc8e15f3aab68b8e21c943367c681
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2022-06-20 12:55:30 +0200

    beest otel: - improve cliv1 fixture

--- beest/tests/otel/conftest.py
+++ beest/tests/otel/conftest.py
@@ -6,31 +6,35 @@ import tempfile as tfile
 
 @pytest.fixture
 def cliv1(host):
-    def topology(query):
-        ctx = "context={{ kubecontext }}"
-        ns = "namespace={{ namespace }}"
-        pod = "pod=stackstate-cli"
+    class CLIv1:
+        def components(self, query):
+            return self.script(f"Topology.query(\"{query}\").components()\n")
 
-        # Transfer query to a file inside the cli pod
-        fd, path = tfile.mkstemp()
-        try:
-            # Write topology query to a temporary file first
-            with os.fdopen(fd, 'w') as tmp_topo_query:
-                # do stuff with temp file
-                tmp_topo_query.write(f"Topology.query(\"{query}\").components()\n")
+        def script(self, fullquery):
+            ctx = "context={{ kubecontext }}"
+            ns = "namespace={{ namespace }}"
+            pod = "pod=stackstate-cli"
 
-            local_path = f"local_path=\"{path}\""
-            remote_path = "remote_path=\"/query.stql\""
-            # then transfer it
-            transferred = host.ansible("kubernetes.core.k8s_cp", f"{ctx} {ns} {pod} {local_path} {remote_path}")["result"]
-            print(f"[cli] transferred query: {transferred}")
-        finally:
-            os.remove(path)
+            # Transfer query to a file inside the cli pod
+            fd, path = tfile.mkstemp()
+            try:
+                # Write topology query to a temporary file first
+                with os.fdopen(fd, 'w') as tmp_topo_query:
+                    # do stuff with temp file
+                    tmp_topo_query.write(fullquery)
 
-        # Execute the query
-        command = f"command=\"bash query.sh\""
-        executed = host.ansible("kubernetes.core.k8s_exec", f"{ctx} {ns} {pod} {command}")["stdout"]
-        print(f"[cli] executed query: {executed}")
-        return json.loads(executed)
+                local_path = f"local_path=\"{path}\""
+                remote_path = "remote_path=\"/query.stql\""
+                # then transfer it
+                transferred = host.ansible("kubernetes.core.k8s_cp", f"{ctx} {ns} {pod} {local_path} {remote_path}")["result"]
+                print(f"[cli] transferred query: {transferred}")
+            finally:
+                os.remove(path)
 
-    return topology
+            # Execute the query
+            command = f"command=\"bash query.sh\""
+            executed = host.ansible("kubernetes.core.k8s_exec", f"{ctx} {ns} {pod} {command}")["stdout"]
+            print(f"[cli] executed query: {executed}")
+            return json.loads(executed)
+
+    return CLIv1()
--- beest/tests/otel/test_topology.py
+++ beest/tests/otel/test_topology.py
@@ -5,7 +5,7 @@ testinfra_hosts = ["local"]
 
 def test_not_empty(cliv1):
     def assert_it():
-        json = cliv1("label IN ('stackpack:aws-v2')")
+        json = cliv1.components("label IN ('stackpack:aws-v2')")
         assert len(json["result"]) > 500
 
     util.wait_until(assert_it, 30, 5)
