16891a7326d542c914ccdaf3e4d91984190183c6
Alexander Hasselbach <agasselbakh@stackstate.com>
2022-06-29 22:34:32 +0300

    STAC-16209 added relation mapping to the topology match result, made some cleanup

--- beest/testframework/ststest/ststest/topology_matcher.py
+++ beest/testframework/ststest/ststest/topology_matcher.py
@@ -6,13 +6,14 @@ from .util import *
 
 
 class TopologyMatch:
-    def __init__(self, components: dict[str, ComponentWrapper], relations: dict[(str, str), ComponentWrapper]):
+    def __init__(self, components: dict[str, ComponentWrapper], relations: dict[(str, str), RelationWrapper]):
         self._components = components
         self._relations = relations
 
     def __repr__(self):
         return "Match[\n\t" \
                 + "\n\t".join([f"{key}: {comp}" for key, comp in self._components.items()]) \
+                + "\n\t" \
                 + "\n\t".join([f"{source} > {target}: {comp}" for (source, target), comp in self._relations.items()]) \
                 + "\n]"
 
@@ -34,6 +35,13 @@ class TopologyMatchingResult:
         if len(self.errors) == 0 and len(self.matches) == 1:
             return self.matches[0]
         return None
+
+
+def get_common_relations(sources: list[ComponentWrapper], targets: list[ComponentWrapper]):
+    # TODO consider BOTH_WAY type of relations
+    source_relations = set([id for source in sources for id in source.outgoing_relations])
+    target_relations = set([id for target in targets for id in target.incoming_relations])
+    return list(source_relations & target_relations)
 
 
 class TopologyMatcher:
@@ -65,13 +73,8 @@ class TopologyMatcher:
         return self
 
     def find(self, topology: TopologyResult) -> TopologyMatchingResult:
-        component_by_id = {}
-        for comp in topology.components:
-            component_by_id[comp.id] = comp
-
-        relation_by_id = {}
-        for relation in topology.relations:
-            relation_by_id[relation.id] = relation
+        component_by_id = {comp.id: comp for comp in topology.components}
+        relation_by_id = {rel.id: rel for rel in topology.relations}
 
         errors = []
 
@@ -83,33 +86,28 @@ class TopologyMatcher:
         # find all matching components and group them by virtual node (id) from a pattern
         matching_components: dict[str, list[ComponentWrapper]] = {}
         for comp_match in self.components:
-            found = False
-            for component in topology.components:
-                if comp_match.match(component):
-                    if comp_match.id not in matching_components:
-                        matching_components[comp_match.id] = [component]
-                    else:
-                        matching_components[comp_match.id].append(component)
-                    found = True
-            if not found:
+            matching = [comp for comp in topology.components if comp_match.match(comp)]
+            if len(matching) == 0:
                 add_error(f"component {comp_match} has not been not found")
+            else:
+                matching_components[comp_match.id] = matching
 
         # tell CGM that for every virtual node (A) there is a list of possible options (A1..An)
         for key, component_candidates in matching_components.items():
             consistent_graph_matcher.add_choice_of_spec([{key: comp.id} for comp in component_candidates])
 
-        # not we are looking for relations (e.g. A1>B2..Ax>By) that possibly represent a defined relation A>B
+        # now we are looking for relations (e.g. A1>B2..Ax>By) that possibly represents a defined relation A>B
         for comp_rel in self.relations:
             source_candidates = matching_components.get(comp_rel.source, [])
             target_candidates = matching_components.get(comp_rel.target, [])
-            assert len(source_candidates) > 0 and len(target_candidates) > 0, \
-                f"relation {comp_rel} has not been found,\n" \
-                f"\tsource candidates:\n\t\t{components_short_print(source_candidates)}\n" \
-                f"\ttarget candidates:\n\t\t{components_short_print(target_candidates)}\n"
+            if len(source_candidates) == 0 or len(target_candidates) == 0:
+                add_error(
+                    f"relation {comp_rel} has not been found,\n"
+                    f"\tsource candidates:\n\t\t{components_short_print(source_candidates)}\n"
+                    f"\ttarget candidates:\n\t\t{components_short_print(target_candidates)}")
+                continue
 
-            source_relations = set([id for source in source_candidates for id in source.outgoing_relations])
-            target_relations = set([id for target in target_candidates for id in target.incoming_relations])
-            relation_candidate_ids = list(source_relations & target_relations)
+            relation_candidate_ids = get_common_relations(source_candidates, target_candidates)
             relation_candidates = [relation_by_id[id] for id in relation_candidate_ids if id in relation_by_id]
             matching_relations = [rel for rel in relation_candidates if comp_rel.match(rel)]
             if len(matching_relations) == 0:
@@ -129,22 +127,17 @@ class TopologyMatcher:
                     for rel in matching_relations
                 ])
 
-        result_graph_specs = consistent_graph_matcher.get_graphs()
-
         def build_topo_match_from_spec(spec: dict) -> TopologyMatch:
             components = {}
             relations = {}
             for key, id in spec.items():
                 if isinstance(key, tuple):
-                    source, target = key
-                    pass
+                    relations[key] = relation_by_id[id]
                 else:
                     components[key] = component_by_id[id]
-            # out_rels = set([rel_id for comp in components.items() for rel_id in comp.outgoing_relations])
-            # in_rels = set([rel_id for comp in components.items() for rel_id in comp.incoming_relations])
-            # relations = [relation_by_id[id] for id in in_rels & out_rels]
-            return TopologyMatch(components, {})  # TODO build matching relations
+            return TopologyMatch(components, relations)
 
+        result_graph_specs = consistent_graph_matcher.get_graphs()
         return TopologyMatchingResult(list(map(build_topo_match_from_spec, result_graph_specs)), errors)
 
 
--- beest/testframework/ststest/tests/test_topology_matcher.py
+++ beest/testframework/ststest/tests/test_topology_matcher.py
@@ -1,93 +1,9 @@
 from unittest import TestCase
-from stscliv1 import TopologyResult, ComponentWrapper, RelationWrapper
 from ststest import TopologyMatcher, TopologyMatch
-
-
-def component_fixture(id: int, name: str, outgoing: list[int] = None, incoming=None) -> ComponentWrapper:
-    return ComponentWrapper({
-        'id': id, 'name': name, 'type': 'component',
-        'incomingRelations': incoming if incoming is not None else [],
-        'outgoingRelations': outgoing if outgoing is not None else [],
-    })
-
-
-def relation_fixture(id: int, source: int, target: int, type: str, direction: str = 'ONE_WAY') -> RelationWrapper:
-    return RelationWrapper({
-        'id': id,
-        'source': source,
-        'target': target,
-        'type': type,
-        'dependencyDirection': direction,
-    })
-
-
-class TopologyFixture:
-    def __init__(self, definition: str):
-        self.elements = {}
-        self.components = []
-        self.relations = []
-
-        element_id = 1
-        component_ids = {}
-        for element_definition in definition.split(","):
-            elem_def_parts = element_definition.split(">")
-            if len(elem_def_parts) == 1:
-                name = elem_def_parts[0]
-                component = component_fixture(element_id, name)
-                component_ids[name] = component
-                self.elements[element_definition] = component
-                self.components.append(component)
-            elif len(elem_def_parts) == 3:
-                source = component_ids[elem_def_parts[0]]
-                type = elem_def_parts[1]
-                target = component_ids[elem_def_parts[2]]
-                relation = relation_fixture(element_id, source.id, target.id, type)
-                self.elements[element_definition] = relation
-                self.relations.append(relation)
-                source.attributes['outgoingRelations'].append(element_id)
-                target.attributes['incomingRelations'].append(element_id)
-            else:
-                raise ValueError(f"unrecognized definition `{element_definition}`, should either `some_name` or `some_a>relates>some_b`")
-            element_id += 1
-
-    def get(self, definition):
-        return self.elements.get(definition)
-
-    def topology(self):
-        return TopologyResult(self.components, self.relations)
+from fixtures import *
 
 
 class TestTopologyMatcher(TestCase):
-    def test_topology_fixture(self):
-        self.assertEqual(TopologyResult(
-            components=[
-                component_fixture(1, 'a')
-            ],
-            relations=[]
-            ), TopologyFixture("a").topology())
-
-        self.assertEqual(TopologyResult(
-            components=[
-                component_fixture(1, 'a', outgoing=[3]),
-                component_fixture(2, 'b', incoming=[3]),
-            ],
-            relations=[
-                relation_fixture(3, 1, 2, 'to'),
-            ]
-        ), TopologyFixture("a,b,a>to>b").topology())
-
-        self.assertEqual(TopologyResult(
-            components=[
-                component_fixture(1, 'a', incoming=[6], outgoing=[3]),
-                component_fixture(2, 'b', incoming=[3], outgoing=[5]),
-                component_fixture(4, 'c', incoming=[5], outgoing=[6]),
-            ],
-            relations=[
-                relation_fixture(3, 1, 2, 'to'),
-                relation_fixture(5, 2, 4, 'goes'),
-                relation_fixture(6, 4, 1, 'backto'),
-            ]
-        ), TopologyFixture("a,b,a>to>b,c,b>goes>c,c>backto>a").topology())
 
     def test_simple_positive(self):
         input_topology = TopologyFixture("a,b,c,a>before>b")
@@ -100,11 +16,9 @@ class TestTopologyMatcher(TestCase):
 
         self.assertEqual(0, len(result.errors), msg="no errors are expected")
         self.assertEqual([TopologyMatch(
-            components={
-                'A': input_topology.get('a'),
-                'B': input_topology.get('b'),
-            },
-            relations={}  # TODO relations are not computed yet
+            components={'A': input_topology.get('a'),
+                        'B': input_topology.get('b')},
+            relations={('A', 'B'): input_topology.get('a>before>b')}
         )], result.matches)
 
     def test_finds_within_multiple_matches(self):
@@ -121,20 +35,18 @@ class TestTopologyMatcher(TestCase):
         self.assertEqual([], result.errors, msg="no errors are expected")
         self.assertEqual([
             TopologyMatch(
-                components={
-                    'A': input_topology.get('a1'),
-                    'B': input_topology.get('b2'),
-                    'C': input_topology.get('c1'),
-                },
-                relations={}  # TODO relations are not computed yet
+                components={'A': input_topology.get('a1'),
+                            'B': input_topology.get('b2'),
+                            'C': input_topology.get('c1')},
+                relations={('A', 'B'): input_topology.get('a1>to>b2'),
+                           ('B', 'C'): input_topology.get('b2>to>c1')}
             ),
             TopologyMatch(
-                components={
-                    'A': input_topology.get('a2'),
-                    'B': input_topology.get('b2'),
-                    'C': input_topology.get('c1'),
-                },
-                relations={}  # TODO relations are not computed yet
+                components={'A': input_topology.get('a2'),
+                            'B': input_topology.get('b2'),
+                            'C': input_topology.get('c1')},
+                relations={('A', 'B'): input_topology.get('a2>to>b2'),
+                           ('B', 'C'): input_topology.get('b2>to>c1')}
             ),
         ], result.matches)
 
@@ -148,4 +60,28 @@ class TestTopologyMatcher(TestCase):
 
         self.assertEqual(1, len(result.errors), msg="no errors expected")
         self.assertRegex(result.errors[0], r"relation.*A.*B")
-        # self.assertEqual([], result.matches, msg="should not have found anything")
+
+    def test_topology_fixture(self):
+        self.assertEqual(TopologyResult(
+            components=[component_fixture(1, 'a')],
+            relations=[]
+        ), TopologyFixture("a").topology())
+
+        self.assertEqual(TopologyResult(
+            components=[component_fixture(1, 'a', outgoing=[3]),
+                        component_fixture(2, 'b', incoming=[3])],
+            relations=[relation_fixture(3, 1, 2, 'to')]
+        ), TopologyFixture("a,b,a>to>b").topology())
+
+        self.assertEqual(TopologyResult(
+            components=[component_fixture(1, 'a', incoming=[6], outgoing=[3]),
+                        component_fixture(2, 'b', incoming=[3, 7], outgoing=[5]),
+                        component_fixture(4, 'c', incoming=[5], outgoing=[6, 7])],
+            relations=[relation_fixture(3, 1, 2, 'to'),
+                       relation_fixture(5, 2, 4, 'goes'),
+                       relation_fixture(6, 4, 1, 'backto'),
+                       relation_fixture(7, 4, 2, 'alsoto')]
+        ), TopologyFixture("a,b,a>to>b,c,b>goes>c,c>backto>a,c>alsoto>b").topology())
+
+        with self.assertRaises(KeyError, msg="forward reference should be rejected in constructor"):
+            TopologyFixture("a,a>to>b,b")
