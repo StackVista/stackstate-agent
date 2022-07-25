8d13a849706c02a5f6ab6e4b5f4fa524c34baa17
Melcom van Eeden <melcomve@outlook.com>
2022-07-25 14:28:35 +0200

    Added more unit tests for state_manager

--- pkg/collector/check/state/state_manager_test.go
+++ pkg/collector/check/state/state_manager_test.go
@@ -1,7 +1,9 @@
 package state
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"os"
 	"testing"
 	"time"
@@ -15,6 +17,36 @@ func TestDefaultCheckStateManagerConfig(t *testing.T) {
 	assert.Equal(t, 10*time.Minute, csm.Config.CachePurgeDuration)
 }
 
+func TestReadFromDisk(t *testing.T) {
+	csm := NewCheckStateManager()
+	diskFileValue, err := csm.readFromDisk("this-key-does-not-exist")
+
+	assert.Equal(t, "{}", diskFileValue)
+	assert.Equal(t, nil, err)
+}
+
+func TestGetFileForKey(t *testing.T) {
+	csm := NewCheckStateManager()
+	filePath, err := csm.getFileForKey("this-key-does-not-exist")
+
+	assert.Equal(t, fmt.Sprintf("%s/this-key-does-not-exist", csm.Config.StateRootPath), filePath)
+	assert.Equal(t, nil, err)
+}
+
+func TestWriteToDisk(t *testing.T) {
+	// Create a directory for the temp write to disk tests
+	// We do not need to check for an error here, if this fails then the rest will
+	var _ = os.Mkdir("temp-write-to-disk-test", os.ModePerm)
+
+	os.Setenv("DD_CHECK_STATE_ROOT_PATH", "./temp-write-to-disk-test")
+
+	// Create a file on the disk
+	csm := NewCheckStateManager()
+	err := csm.writeToDisk(fmt.Sprintf("temp-write-to-disk-test-%f", rand.Float64()), "random-data")
+
+	assert.Equal(t, nil, err)
+}
+
 func TestCheckStateManagerConfigOverrides(t *testing.T) {
 	os.Setenv("DD_CHECK_STATE_ROOT_PATH", "/my/custom/path")
 	os.Setenv("DD_CHECK_STATE_EXPIRATION_DURATION", "5m")
@@ -25,6 +57,28 @@ func TestCheckStateManagerConfigOverrides(t *testing.T) {
 	assert.Equal(t, "/my/custom/path", csm.Config.StateRootPath)
 	assert.Equal(t, 5*time.Minute, csm.Config.CacheExpirationDuration)
 	assert.Equal(t, 15*time.Minute, csm.Config.CachePurgeDuration)
+}
+
+func TestCheckStateManager_NonExistentGetState(t *testing.T) {
+	os.Setenv("DD_CHECK_STATE_ROOT_PATH", "./file-does-not-exist")
+
+	csm := NewCheckStateManager()
+
+	stateKey := "random-state-key"
+	expectedState := "{}"
+
+	state, err := csm.GetState(stateKey)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedState, state)
+
+	// call GetState again, thereby fetching it from the cache
+	state, err = csm.GetState(stateKey)
+	assert.NoError(t, err, "unexpected error occurred when getting state for %s", stateKey)
+	assert.Equal(t, expectedState, state)
+
+	actualState, found := csm.Cache.Get(stateKey)
+	assert.True(t, found, "%s not found in the check state manager cache", stateKey)
+	assert.Equal(t, expectedState, actualState)
 }
 
 func TestCheckStateManager_GetState(t *testing.T) {
