DataDog/datadog-agent

Isolate each core check in its dedicated sub-package (#7433)

pkg/collector/corechecks/containers/docker/docker_test.go
@@ -1,0 +1,137 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// +build docker,!darwin
+
+package docker
+
+import (
+	"testing"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
+	cmetrics "github.com/DataDog/datadog-agent/pkg/util/containers/metrics"
+)
+
+func TestReportIOMetrics(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+
+	// Test fallback to sums when per-device is not available
+	ioSum := &cmetrics.ContainerIOStats{
+		ReadBytes:  uint64(38989367),
+		WriteBytes: uint64(671846455),
+	}
+	dockerCheck.reportIOMetrics(ioSum, tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_bytes", float64(38989367), "", tags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_bytes", float64(671846455), "", tags)
+
+	// Test per-device when available
+	ioPerDevice := &cmetrics.ContainerIOStats{
+		ReadBytes:  uint64(38989367),
+		WriteBytes: uint64(671846455),
+		DeviceReadBytes: map[string]uint64{
+			"sda": 37858816,
+			"sdb": 1130496,
+		},
+		DeviceWriteBytes: map[string]uint64{
+			"sda": 671846400,
+			"sdb": 0,
+		},
+		DeviceReadOperations: map[string]uint64{
+			"sda": 1042,
+			"sdb": 42,
+		},
+		DeviceWriteOperations: map[string]uint64{
+			"sda": 2042,
+			"sdb": 1042,
+		},
+	}
+	sdaTags := append(tags, "device:sda", "device_name:sda")
+	sdbTags := append(tags, "device:sdb", "device_name:sdb")
+	dockerCheck.reportIOMetrics(ioPerDevice, tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_bytes", float64(37858816), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_bytes", float64(671846400), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_bytes", float64(1130496), "", sdbTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_bytes", float64(0), "", sdbTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_operations", float64(1042), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_operations", float64(2042), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_operations", float64(42), "", sdbTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_operations", float64(1042), "", sdbTags)
+}
+
+func TestReportUptime(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+	currentTime := time.Now().Unix()
+
+	startTime := currentTime - 60
+	dockerCheck.reportUptime(startTime, currentTime, tags, mockSender)
+	mockSender.AssertMetric(t, "Gauge", "docker.uptime", 60.0, "", tags)
+}
+
+func TestReportCPUNoLimit(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+	testTime := time.Now()
+	startTime := testTime.Add(-10 * time.Second)
+
+	cpu := cmetrics.ContainerCPUStats{
+		Timestsamp: testTime,
+		System:     10,
+		User:       10,
+		UsageTotal: 20.0,
+	}
+
+	// 100% is 1 CPU, 200% is 2 CPUs, etc.
+	// So no limit is # of host CPU
+	limits := cmetrics.ContainerLimits{
+		CPULimit: 100.0,
+	}
+
+	dockerCheck.reportCPUMetrics(&cpu, &limits, startTime.Unix(), tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.cpu.limit", 1000, "", tags)
+}
+
+func TestReportCPULimit(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+	testTime := time.Now()
+	startTime := testTime.Add(-10 * time.Second)
+
+	cpu := cmetrics.ContainerCPUStats{
+		Timestsamp: testTime,
+		System:     10,
+		User:       10,
+		UsageTotal: 20.0,
+	}
+
+	limits := cmetrics.ContainerLimits{
+		CPULimit: 50,
+	}
+
+	dockerCheck.reportCPUMetrics(&cpu, &limits, startTime.Unix(), tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.cpu.limit", 500, "", tags)
+}

pkg/collector/corechecks/system/disk/iostats.go
@@ -1,0 +1,71 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package disk
+
+import (
+	"regexp"
+
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
+	"github.com/DataDog/datadog-agent/pkg/collector/check"
+	core "github.com/DataDog/datadog-agent/pkg/collector/corechecks"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+const (
+	// SectorSize is exported in github.com/shirou/gopsutil/disk (but not working!)
+	SectorSize       = 512
+	kB               = (1 << 10)
+	iostatsCheckName = "io"
+)
+
+// Configure the IOstats check
+func (c *IOCheck) commonConfigure(data integration.Data, initConfig integration.Data, source string) error {
+	if err := c.CommonConfigure(data, source); err != nil {
+		return err
+	}
+
+	conf := make(map[interface{}]interface{})
+
+	err := yaml.Unmarshal([]byte(initConfig), &conf)
+	if err != nil {
+		return err
+	}
+
+	blacklistRe, ok := conf["device_exclude_re"]
+	if !ok {
+		blacklistRe, ok = conf["device_blacklist_re"]
+		if ok {
+			log.Warn("'device_blacklist_re' has been deprecated, use 'device_exclude_re' instead")
+		}
+	}
+	if ok && blacklistRe != "" {
+		if regex, ok := blacklistRe.(string); ok {
+			c.blacklist, err = regexp.Compile(regex)
+		}
+	}
+
+	if lowercaseDeviceTagOption, ok := conf["lowercase_device_tag"]; ok {
+		if lowercaseDeviceTag, ok := lowercaseDeviceTagOption.(bool); ok {
+			c.lowercaseDeviceTag = lowercaseDeviceTag
+		} else {
+			log.Warn("Can't cast value of 'lowercase_device_tag' option to boolean: ", lowercaseDeviceTagOption)
+		}
+	}
+
+	return err
+}
+
+func init() {
+	core.RegisterCheck(iostatsCheckName, ioFactory)
+}
+
+func ioFactory() check.Check {
+	return &IOCheck{
+		CheckBase: core.NewCheckBase(iostatsCheckName),
+	}
+}

pkg/collector/corechecks/system/disk/iostats_nix_test.go
@@ -1,0 +1,117 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// +build !windows
+
+package disk
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/stretchr/testify/assert"
+)
+
+var currentStats = map[string]disk.IOCountersStat{
+	"sda": {
+		ReadCount:        41,
+		MergedReadCount:  41,
+		WriteCount:       41,
+		MergedWriteCount: 41,
+		ReadBytes:        42 * kB,
+		WriteBytes:       42 * kB,
+		ReadTime:         41,
+		WriteTime:        41,
+		IopsInProgress:   0,
+		IoTime:           41,
+		WeightedIO:       42 * kB,
+		Name:             "sda",
+		SerialNumber:     "123456789WD",
+	},
+}
+
+var lastStats = map[string]disk.IOCountersStat{
+	"sda": {
+		ReadCount:        maxULong,
+		MergedReadCount:  maxULong,
+		WriteCount:       maxULong,
+		MergedWriteCount: maxULong,
+		ReadBytes:        maxULong,
+		WriteBytes:       maxULong,
+		ReadTime:         uint64(math.MaxUint32),
+		WriteTime:        uint64(math.MaxUint32),
+		IopsInProgress:   0,
+		IoTime:           maxULong,
+		WeightedIO:       uint64(math.MaxUint32),
+		Name:             "sda",
+		SerialNumber:     "123456789WD",
+	},
+}
+
+func TestOverflow32(t *testing.T) {
+	increment := incrementWithOverflow(0, math.MaxUint32)
+	assert.Equal(t, int64(1), increment)
+}
+
+func TestOverflow64(t *testing.T) {
+	increment := incrementWithOverflow(0, math.MaxUint64)
+	assert.Equal(t, int64(1), increment)
+}
+
+func TestWithRealValues32(t *testing.T) {
+	increment := incrementWithOverflow(123456, math.MaxUint32-2)
+	assert.Equal(t, int64(123459), increment)
+}
+
+func TestWithRealValues64(t *testing.T) {
+	increment := incrementWithOverflow(123456, math.MaxUint64-2)
+	assert.Equal(t, int64(123459), increment)
+}
+
+func TestIncrementWithOverflow(t *testing.T) {
+	assert.Equal(t, int64(1), incrementWithOverflow(maxULong-1, maxULong-2))
+	assert.Equal(t, int64(1), incrementWithOverflow(maxULong, maxULong-1))
+	assert.Equal(t, int64(1), incrementWithOverflow(0, maxULong))
+	assert.Equal(t, int64(1), incrementWithOverflow(1, 0))
+}
+
+func TestIoStatsOverflow(t *testing.T) {
+
+	ioCheck := new(IOCheck)
+	ioCheck.Configure(nil, nil, "test")
+	ioCheck.stats = lastStats
+	ioCheck.ts = 1000
+	ioCounters = func(names ...string) (map[string]disk.IOCountersStat, error) {
+		return currentStats, nil
+	}
+
+	mock := mocksender.NewMockSender(ioCheck.ID())
+
+	mock.On("Rate", "system.io.r_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Rate", "system.io.w_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Rate", "system.io.rrqm_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Rate", "system.io.wrqm_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.rkb_s", 42.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.wkb_s", 42.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.avg_rq_sz", 2.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.await", 1.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.r_await", 1.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.w_await", 1.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.avg_q_sz", 42.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.util", 4.2, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.svctm", 0.5, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Commit").Return().Times(1)
+
+	// simulate a 1s interval
+	nowNano = func() int64 { return 2000 * 1000000 } // time of the second run
+	defer func() { nowNano = time.Now().UnixNano }()
+
+	ioCheck.Run()
+
+	mock.AssertExpectations(t)
+}

StackVista/stackstate-agent

pkg/collector/corechecks/containers/docker/docker_test.go
@@ -1,0 +1,137 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// +build docker,!darwin
+
+package docker
+
+import (
+	"testing"
+	"time"
+
+	"github.com/StackVista/stackstate-agent/pkg/aggregator/mocksender"
+	cmetrics "github.com/StackVista/stackstate-agent/pkg/util/containers/metrics"
+)
+
+func TestReportIOMetrics(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+
+	// Test fallback to sums when per-device is not available
+	ioSum := &cmetrics.ContainerIOStats{
+		ReadBytes:  uint64(38989367),
+		WriteBytes: uint64(671846455),
+	}
+	dockerCheck.reportIOMetrics(ioSum, tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_bytes", float64(38989367), "", tags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_bytes", float64(671846455), "", tags)
+
+	// Test per-device when available
+	ioPerDevice := &cmetrics.ContainerIOStats{
+		ReadBytes:  uint64(38989367),
+		WriteBytes: uint64(671846455),
+		DeviceReadBytes: map[string]uint64{
+			"sda": 37858816,
+			"sdb": 1130496,
+		},
+		DeviceWriteBytes: map[string]uint64{
+			"sda": 671846400,
+			"sdb": 0,
+		},
+		DeviceReadOperations: map[string]uint64{
+			"sda": 1042,
+			"sdb": 42,
+		},
+		DeviceWriteOperations: map[string]uint64{
+			"sda": 2042,
+			"sdb": 1042,
+		},
+	}
+	sdaTags := append(tags, "device:sda", "device_name:sda")
+	sdbTags := append(tags, "device:sdb", "device_name:sdb")
+	dockerCheck.reportIOMetrics(ioPerDevice, tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_bytes", float64(37858816), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_bytes", float64(671846400), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_bytes", float64(1130496), "", sdbTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_bytes", float64(0), "", sdbTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_operations", float64(1042), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_operations", float64(2042), "", sdaTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.read_operations", float64(42), "", sdbTags)
+	mockSender.AssertMetric(t, "Rate", "docker.io.write_operations", float64(1042), "", sdbTags)
+}
+
+func TestReportUptime(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+	currentTime := time.Now().Unix()
+
+	startTime := currentTime - 60
+	dockerCheck.reportUptime(startTime, currentTime, tags, mockSender)
+	mockSender.AssertMetric(t, "Gauge", "docker.uptime", 60.0, "", tags)
+}
+
+func TestReportCPUNoLimit(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+	testTime := time.Now()
+	startTime := testTime.Add(-10 * time.Second)
+
+	cpu := cmetrics.ContainerCPUStats{
+		Timestsamp: testTime,
+		System:     10,
+		User:       10,
+		UsageTotal: 20.0,
+	}
+
+	// 100% is 1 CPU, 200% is 2 CPUs, etc.
+	// So no limit is # of host CPU
+	limits := cmetrics.ContainerLimits{
+		CPULimit: 100.0,
+	}
+
+	dockerCheck.reportCPUMetrics(&cpu, &limits, startTime.Unix(), tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.cpu.limit", 1000, "", tags)
+}
+
+func TestReportCPULimit(t *testing.T) {
+	dockerCheck := &DockerCheck{
+		instance: &DockerConfig{},
+	}
+	mockSender := mocksender.NewMockSender(dockerCheck.ID())
+	mockSender.SetupAcceptAll()
+
+	tags := []string{"constant:tags", "container_name:dummy"}
+	testTime := time.Now()
+	startTime := testTime.Add(-10 * time.Second)
+
+	cpu := cmetrics.ContainerCPUStats{
+		Timestsamp: testTime,
+		System:     10,
+		User:       10,
+		UsageTotal: 20.0,
+	}
+
+	limits := cmetrics.ContainerLimits{
+		CPULimit: 50,
+	}
+
+	dockerCheck.reportCPUMetrics(&cpu, &limits, startTime.Unix(), tags, mockSender)
+	mockSender.AssertMetric(t, "Rate", "docker.cpu.limit", 500, "", tags)
+}

pkg/collector/corechecks/system/disk/iostats.go
@@ -1,0 +1,71 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package disk
+
+import (
+	"regexp"
+
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/StackVista/stackstate-agent/pkg/autodiscovery/integration"
+	"github.com/StackVista/stackstate-agent/pkg/collector/check"
+	core "github.com/StackVista/stackstate-agent/pkg/collector/corechecks"
+	"github.com/StackVista/stackstate-agent/pkg/util/log"
+)
+
+const (
+	// SectorSize is exported in github.com/shirou/gopsutil/disk (but not working!)
+	SectorSize       = 512
+	kB               = (1 << 10)
+	iostatsCheckName = "io"
+)
+
+// Configure the IOstats check
+func (c *IOCheck) commonConfigure(data integration.Data, initConfig integration.Data, source string) error {
+	if err := c.CommonConfigure(data, source); err != nil {
+		return err
+	}
+
+	conf := make(map[interface{}]interface{})
+
+	err := yaml.Unmarshal([]byte(initConfig), &conf)
+	if err != nil {
+		return err
+	}
+
+	blacklistRe, ok := conf["device_exclude_re"]
+	if !ok {
+		blacklistRe, ok = conf["device_blacklist_re"]
+		if ok {
+			log.Warn("'device_blacklist_re' has been deprecated, use 'device_exclude_re' instead")
+		}
+	}
+	if ok && blacklistRe != "" {
+		if regex, ok := blacklistRe.(string); ok {
+			c.blacklist, err = regexp.Compile(regex)
+		}
+	}
+
+	if lowercaseDeviceTagOption, ok := conf["lowercase_device_tag"]; ok {
+		if lowercaseDeviceTag, ok := lowercaseDeviceTagOption.(bool); ok {
+			c.lowercaseDeviceTag = lowercaseDeviceTag
+		} else {
+			log.Warn("Can't cast value of 'lowercase_device_tag' option to boolean: ", lowercaseDeviceTagOption)
+		}
+	}
+
+	return err
+}
+
+func init() {
+	core.RegisterCheck(iostatsCheckName, ioFactory)
+}
+
+func ioFactory() check.Check {
+	return &IOCheck{
+		CheckBase: core.NewCheckBase(iostatsCheckName),
+	}
+}

pkg/collector/corechecks/system/disk/iostats_nix_test.go
@@ -1,0 +1,117 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// +build !windows
+
+package disk
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/StackVista/stackstate-agent/pkg/aggregator/mocksender"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/stretchr/testify/assert"
+)
+
+var currentStats = map[string]disk.IOCountersStat{
+	"sda": {
+		ReadCount:        41,
+		MergedReadCount:  41,
+		WriteCount:       41,
+		MergedWriteCount: 41,
+		ReadBytes:        42 * kB,
+		WriteBytes:       42 * kB,
+		ReadTime:         41,
+		WriteTime:        41,
+		IopsInProgress:   0,
+		IoTime:           41,
+		WeightedIO:       42 * kB,
+		Name:             "sda",
+		SerialNumber:     "123456789WD",
+	},
+}
+
+var lastStats = map[string]disk.IOCountersStat{
+	"sda": {
+		ReadCount:        maxULong,
+		MergedReadCount:  maxULong,
+		WriteCount:       maxULong,
+		MergedWriteCount: maxULong,
+		ReadBytes:        maxULong,
+		WriteBytes:       maxULong,
+		ReadTime:         uint64(math.MaxUint32),
+		WriteTime:        uint64(math.MaxUint32),
+		IopsInProgress:   0,
+		IoTime:           maxULong,
+		WeightedIO:       uint64(math.MaxUint32),
+		Name:             "sda",
+		SerialNumber:     "123456789WD",
+	},
+}
+
+func TestOverflow32(t *testing.T) {
+	increment := incrementWithOverflow(0, math.MaxUint32)
+	assert.Equal(t, int64(1), increment)
+}
+
+func TestOverflow64(t *testing.T) {
+	increment := incrementWithOverflow(0, math.MaxUint64)
+	assert.Equal(t, int64(1), increment)
+}
+
+func TestWithRealValues32(t *testing.T) {
+	increment := incrementWithOverflow(123456, math.MaxUint32-2)
+	assert.Equal(t, int64(123459), increment)
+}
+
+func TestWithRealValues64(t *testing.T) {
+	increment := incrementWithOverflow(123456, math.MaxUint64-2)
+	assert.Equal(t, int64(123459), increment)
+}
+
+func TestIncrementWithOverflow(t *testing.T) {
+	assert.Equal(t, int64(1), incrementWithOverflow(maxULong-1, maxULong-2))
+	assert.Equal(t, int64(1), incrementWithOverflow(maxULong, maxULong-1))
+	assert.Equal(t, int64(1), incrementWithOverflow(0, maxULong))
+	assert.Equal(t, int64(1), incrementWithOverflow(1, 0))
+}
+
+func TestIoStatsOverflow(t *testing.T) {
+
+	ioCheck := new(IOCheck)
+	ioCheck.Configure(nil, nil, "test")
+	ioCheck.stats = lastStats
+	ioCheck.ts = 1000
+	ioCounters = func(names ...string) (map[string]disk.IOCountersStat, error) {
+		return currentStats, nil
+	}
+
+	mock := mocksender.NewMockSender(ioCheck.ID())
+
+	mock.On("Rate", "system.io.r_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Rate", "system.io.w_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Rate", "system.io.rrqm_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Rate", "system.io.wrqm_s", 41.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.rkb_s", 42.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.wkb_s", 42.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.avg_rq_sz", 2.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.await", 1.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.r_await", 1.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.w_await", 1.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.avg_q_sz", 42.0, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.util", 4.2, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Gauge", "system.io.svctm", 0.5, "", []string{"device:sda", "device_name:sda"}).Return().Times(1)
+	mock.On("Commit").Return().Times(1)
+
+	// simulate a 1s interval
+	nowNano = func() int64 { return 2000 * 1000000 } // time of the second run
+	defer func() { nowNano = time.Now().UnixNano }()
+
+	ioCheck.Run()
+
+	mock.AssertExpectations(t)
+}