ed9b940f873499bfda9e66223da9b78654a681b8
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2022-08-03 09:34:53 +0200

    beest: sets VantaOwner to stackstate@stackstate.com

--- beest/sut/hives/ec2-agent/main.tf
+++ beest/sut/hives/ec2-agent/main.tf
@@ -78,7 +78,7 @@ resource "aws_instance" "agent" {
     VantaContainsUserData = false
     VantaDescription      = "Machine used used in acceptance pipeline"
     VantaNonProd          = true
-    VantaOwner            = "beest@stackstate.com"
+    VantaOwner            = "stackstate@stackstate.com"
     VantaUserDataStored   = "NA"
   }
 }
