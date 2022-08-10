ed9fe4b85833293aeeff7e450a59b44c2486e179
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2022-08-10 14:46:41 +0200

    beest k8s: - switch from receiver docker-compose to stackstate running in sandbox cluster - connect to sandbox with sts-toolbox - use proper ansible modules instead of command

---
+++ beest/sut/yards/k8s/outputs.tf
@@ -0,0 +1,45 @@
+resource "local_file" "agent_eks_aws_auth" {
+  filename        = "${path.module}/agent_eks_aws_auth.yml"
+  content         = module.eks_cluster.k8s_aws_auth
+  file_permission = "0600"
+}
+
+resource "local_file" "get_kubeconfig" {
+  filename        = "${path.module}/get_kubeconfig.sh"
+  content         = <<KUBECONFIG
+#!/bin/bash
+set -euo pipefail
+
+echo "Get kubeconfig for agent cluster ${local.agent_cluster_name}"
+aws eks update-kubeconfig --name ${local.agent_cluster_name} --alias ${var.yard_id}
+
+echo "Configure kubernetes agent cluster AWS authentication"
+kubectl --context ${var.yard_id} apply -f ${local_file.agent_eks_aws_auth.filename}
+
+echo "Get kubeconfig for stackstate sandbox cluster"
+sts-toolbox cluster connect sandbox-main.sandbox.stackstate.io
+KUBECONFIG
+  file_permission = "0770"
+}
+
+//
+
+resource "local_file" "ansible_inventory" {
+  filename        = "${path.module}/ansible_inventory"
+  content         = <<INVENTORY
+[local]
+localhost ansible_connection=local
+
+[all:vars]
+yard_id=${var.yard_id}
+agent_k8s_runtime=${var.agent_eks_runtime}
+agent_k8s_version=${var.agent_eks_version}
+INVENTORY
+  file_permission = "0777"
+}
+
+resource "local_file" "agent_eks_node_id_rsa" {
+  filename        = "${path.module}/agent_eks_node_id_rsa"
+  content         = module.eks_cluster.node_ssh_key
+  file_permission = "0600"
+}
