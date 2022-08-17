078569da349c80f87306528afcb0bc6f4253e66e
Juliano Krieger <julianosk@gmail.com>
2022-08-17 15:23:05 -0300

    STAC-16866: Add kubeapi checks to cluster-agent
    Signed-off-by: Juliano Krieger <julianosk@gmail.com>

--- cmd/cluster-agent/main.go
+++ cmd/cluster-agent/main.go
@@ -17,6 +17,7 @@ import (
 	_ "net/http/pprof" // Blank import used because this isn't directly used in this file
 
 	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/cluster/ksm"
+	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/cluster/kubeapi" // sts
 	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/cluster/kubernetesapiserver"
 	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/cluster/orchestrator"
 	_ "github.com/StackVista/stackstate-agent/pkg/collector/corechecks/net"
