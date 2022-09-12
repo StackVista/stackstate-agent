09e00b2a437ef42fe2bce1e1a8b7dffe349d7822
Zandré van Heerden <zvheerden@stackstate.com>
2022-09-12 11:26:26 +0200

    STAC-0 Pipeline update

---
+++ gitlab-ci-pipeline-check.py
@@ -0,0 +1,23 @@
+from operator import truediv
+import os
+import sys
+
+run_full_pipeline = True
+
+for i in range(1, len(sys.argv)):
+    if ".md" in str(sys.argv[i]) or ".markdown" in str(sys.argv[i]) or ".mdown" in str(sys.argv[i]):
+        print("md file changed")
+        run_full_pipeline = False
+    else:
+        run_full_pipeline = True
+        break
+    print('argument:', i, 'value:', sys.argv[i])
+
+print(run_full_pipeline)
+
+if run_full_pipeline:
+    with open('.env', 'w') as writer:
+        writer.write(f'export RUN_FULL_PIPELINE="true"')
+else:
+    with open('.env', 'w') as writer:
+        writer.write(f'export RUN_FULL_PIPELINE="false"')
