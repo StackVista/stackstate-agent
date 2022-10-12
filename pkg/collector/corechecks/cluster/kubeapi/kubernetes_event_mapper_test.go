d120d33bc8295fe599195b4ede5e73cb6d69599e
Zandré van Heerden <zvheerden@stackstate.com>
2022-10-12 15:14:13 +0200

    STAC-17465 Updated unit tests related to k8s events

--- pkg/collector/corechecks/cluster/kubeapi/kubernetes_event_mapper_test.go
+++ pkg/collector/corechecks/cluster/kubeapi/kubernetes_event_mapper_test.go
@@ -13,9 +13,13 @@ import (
 )
 
 func TestEventTimestamps(t *testing.T) {
-	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
-	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662800, "Alerts")
-	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662800, 709662600, "Alerts")
+
+	firstTimestamp := int64(709662600)
+	laterTimeSTamp := int64(709662800)
+
+	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, firstTimestamp, "Alerts")
+	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
+	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", laterTimeSTamp, firstTimestamp, "Alerts")
 
 	mapper := &kubernetesEventMapper{
 		urn:                     urn.NewURNBuilder(urn.Kubernetes, "testCluster"),
@@ -28,38 +32,52 @@ func TestEventTimestamps(t *testing.T) {
 	mEvent2, _ := mapper.mapKubernetesEvent(event2)
 	mEvent3, _ := mapper.mapKubernetesEvent(event3)
 
-	assert.Equal(t, int64(709662600), mEvent1.Ts)
-	assert.Equal(t, int64(709662800), mEvent2.Ts)
-	assert.Equal(t, int64(709662600), mEvent3.Ts)
+	assert.Equal(t, firstTimestamp, mEvent1.Ts)
+	assert.Equal(t, laterTimeSTamp, mEvent2.Ts)
+	assert.Equal(t, firstTimestamp, mEvent3.Ts)
 }
 
 func TestContainerNameFromEvent(t *testing.T) {
-	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
-	event1.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{stackstate-agent}}"
 
-	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
-	event2.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{cluster-agent}}"
+	firstTimestamp := int64(709662600)
+	laterTimeSTamp := int64(709662800)
 
-	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
+	eventContainer1 := "stackstate-agent"
+	eventContainer2 := "cluster-agent"
+
+	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
+	event1.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{" + eventContainer1 + "}}"
+
+	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
+	event2.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{" + eventContainer2 + "}}"
+
+	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
 	event3.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017}"
 
 	containerName1 := getContainerNameFromEvent(event1)
 	containerName2 := getContainerNameFromEvent(event2)
 	containerName3 := getContainerNameFromEvent(event3)
 
-	assert.Equal(t, "stackstate-agent", containerName1)
-	assert.Equal(t, "cluster-agent", containerName2)
+	assert.Equal(t, eventContainer1, containerName1)
+	assert.Equal(t, eventContainer2, containerName2)
 	assert.Equal(t, "", containerName3)
 }
 
 func TestEventTags(t *testing.T) {
-	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
-	event1.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{stackstate-agent}}"
 
-	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
-	event2.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{cluster-agent}}"
+	firstTimestamp := int64(709662600)
+	laterTimeSTamp := int64(709662800)
 
-	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
+	eventContainer1 := "stackstate-agent"
+	eventContainer2 := "cluster-agent"
+
+	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
+	event1.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{" + eventContainer1 + "}}"
+
+	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
+	event2.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{" + eventContainer2 + "}}"
+
+	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
 	event3.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017}"
 
 	mapper := &kubernetesEventMapper{
@@ -73,19 +91,26 @@ func TestEventTags(t *testing.T) {
 	eventTags2 := mapper.getTags(event2)
 	eventTags3 := mapper.getTags(event3)
 
-	assert.Contains(t, eventTags1, "kube_container_name:stackstate-agent")
-	assert.Contains(t, eventTags2, "kube_container_name:cluster-agent")
+	assert.Contains(t, eventTags1, "kube_container_name:"+eventContainer1)
+	assert.Contains(t, eventTags2, "kube_container_name:"+eventContainer2)
 	assert.NotContains(t, eventTags3, "kube_container_name:")
 }
 
 func TestEventElementIdentifiers(t *testing.T) {
-	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
-	event1.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{stackstate-agent}}"
 
-	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
-	event2.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{cluster-agent}}"
+	firstTimestamp := int64(709662600)
+	laterTimeSTamp := int64(709662800)
 
-	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", 709662600, 709662600, "Alerts")
+	eventContainer1 := "stackstate-agent"
+	eventContainer2 := "cluster-agent"
+
+	event1 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
+	event1.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{" + eventContainer1 + "}}"
+
+	event2 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
+	event2.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017 spec.containers{" + eventContainer2 + "}}"
+
+	event3 := createEvent(2, "default", "dca-789976f5d7-2ljx6", "Pod", "e6417a7f-f566-11e7-9749-0e4863e1cbf4", "default-scheduler", "machine-blue", "Unhealthy", "Liveness probe errored:", firstTimestamp, laterTimeSTamp, "Alerts")
 	event3.InvolvedObject.FieldPath = "{Pod default stackstate-agent-checks-agent-8f65d8466-w89vb f107ac9f-5d15-495e-8953-781bbe23c2f2 v1 5017}"
 
 	mapper := &kubernetesEventMapper{
@@ -100,11 +125,11 @@ func TestEventElementIdentifiers(t *testing.T) {
 	elementIdentifier3 := mapper.externalIdentifierForInvolvedObject(event3)
 
 	assert.Equal(t, 2, len(elementIdentifier1))
-	assert.Contains(t, elementIdentifier1, "urn:kubernetes:/testCluster:default:pod/dca-789976f5d7-2ljx6:container/stackstate-agent")
+	assert.Contains(t, elementIdentifier1, "urn:kubernetes:/testCluster:default:pod/dca-789976f5d7-2ljx6:container/"+eventContainer1)
 	assert.Contains(t, elementIdentifier1, "urn:kubernetes:/testCluster:default:pod/dca-789976f5d7-2ljx6")
 
 	assert.Equal(t, 2, len(elementIdentifier2))
-	assert.Contains(t, elementIdentifier2, "urn:kubernetes:/testCluster:default:pod/dca-789976f5d7-2ljx6:container/cluster-agent")
+	assert.Contains(t, elementIdentifier2, "urn:kubernetes:/testCluster:default:pod/dca-789976f5d7-2ljx6:container/"+eventContainer2)
 	assert.Contains(t, elementIdentifier2, "urn:kubernetes:/testCluster:default:pod/dca-789976f5d7-2ljx6")
 
 	assert.Equal(t, 1, len(elementIdentifier3))
