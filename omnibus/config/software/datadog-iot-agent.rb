6a1b990052e0ff87182eba50c401f81b5f7581aa
Louis Parkin <lparkin@stackstate.com>
2022-10-12 16:05:07 +0200

    STAC-16866 Changes for upstream runner.
    STAC-16866 sed rename of ddtray to ststray (new files, upstream merge)
    
    STAC-0 Ensure artifacts always expire.
    
    
    STAC-16866 Attempting something
    
    STAC-16866 Added more branding changes, also prevent build from defaulting to python 3.
    
    
    STAC-16866 Remove processing of security-agent.
    
    
    STAC-16866 Additional branding changes.
    
    
    STAC-16866 Fix for python dll files and further exclusions of security agent.
    
    
    STAC-16866 fixed some sed globbing, also temporarily removed agent3 pipeline to speed up testing.
    
    
    STAC-16866 Put agent3 back.
    
    
    STAC-16866 Attempt some cleanup.
    
    
    STAC-16866 Some more attempted cleanup.
    
    
    STAC-16866 Update omnibus hashes.
    
    
    STAC-16866 Kill cleanup task, add it to build job.
    
    
    STAC-16866 Enabled long paths on windows.
    
    https://devopsjournal.io/blog/2020/09/23/Git-clone-Filename-too-long-Windows
    
    STAC-16866 Cannot cleanup because cache step runs after "after_script" step.
    
    
    STAC-16866 Cannot cleanup because artifact upload happens after "after_script" step.
    
    
    STAC-16866 Cannot use pre_clone_script, as it is runner config (will execute for every job, not desired).

--- omnibus/config/software/datadog-iot-agent.rb
+++ omnibus/config/software/datadog-iot-agent.rb
@@ -127,8 +127,9 @@ build do
       platform = windows_arch_i386? ? "x86" : "x64"
       # Build the security-agent with the correct go version for windows
       command "invoke -e security-agent.build --major-version #{major_version_arg} --arch #{platform}", :env => env
-
-      copy 'bin/security-agent/security-agent.exe', "#{Omnibus::Config.source_dir()}/datadog-iot-agent/src/github.com/DataDog/datadog-agent/bin/agent"
+      if $enable_security_agent
+        copy 'bin/security-agent/security-agent.exe', "#{Omnibus::Config.source_dir()}/datadog-iot-agent/src/github.com/DataDog/datadog-agent/bin/agent"
+      end
     end
   end
 
