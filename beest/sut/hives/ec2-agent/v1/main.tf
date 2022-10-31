f3bb9c613be1e2d125b1d7447ca1c76861492b1d
Juliano Krieger <julianosk@gmail.com>
2022-10-31 14:05:40 +0100

    STAC-18217: Enable SSH from NAT gateway of tooling account
    Signed-off-by: Juliano Krieger <julianosk@gmail.com>

--- beest/sut/hives/ec2-agent/v1/main.tf
+++ beest/sut/hives/ec2-agent/v1/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "agent_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.local_ip.body)}/32"]
+    cidr_blocks = ["54.220.174.98/32"]
   }
 
   ingress {
