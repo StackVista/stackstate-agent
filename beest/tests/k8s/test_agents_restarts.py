8c2ddeb336902ed742d406a13e11b94467ea8eb9
Juliano Krieger <julianosk@gmail.com>
2022-12-08 09:52:44 +0100

    STAC-18515: Apply MR suggestions
    Signed-off-by: Juliano Krieger <julianosk@gmail.com>

--- beest/tests/k8s/test_agents_restarts.py
+++ beest/tests/k8s/test_agents_restarts.py
@@ -2,8 +2,8 @@ testinfra_hosts = ["local"]
 
 
 def _get_pod_restarts(kubecontext, host):
-    jsonpath = "'{range .items[*]}|{.metadata.name}={range .status.containerStatuses[*]},{.restartCount}'"
-    cmd = host.run("kubectl --context={0} get pod -o jsonpath={1}".format(kubecontext, jsonpath))
+    jsonpath = "{range .items[*]}|{.metadata.name}={range .status.containerStatuses[*]},{.restartCount}"
+    cmd = host.run("kubectl --context={0} get pod -o jsonpath='{1}'".format(kubecontext, jsonpath))
 
     assert cmd.rc == 0
     restarts = cmd.stdout.split("|")
@@ -19,4 +19,4 @@ def test_agents_do_not_restart(host, ansible_var):
             pod_name, restarts = pod_split[0], pod_split[1].split(",")
             for restart in restarts:
                 if restart.isnumeric():
-                    assert int(restart) < 0, "pod {} has a container with {} restarts".format(pod_name, restart)
+                    assert int(restart) == 0, "pod {} has a container with {} restarts".format(pod_name, restart)
