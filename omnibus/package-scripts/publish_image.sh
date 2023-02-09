a9ee8b782ae5d00202197c93328b25b271efbf0a
Louis Parkin <lparkin@stackstate.com>
2023-02-09 09:57:46 +0100

    STAC-19061 Removed dockerhub push destination.

--- omnibus/package-scripts/publish_image.sh
+++ omnibus/package-scripts/publish_image.sh
@@ -7,8 +7,7 @@ IMAGE_REPO="${2}"
 DOCKERFILE_PATH="${3}"
 EXTRA_TAG="${4}"
 K8S_REPO="${5}"
-REGISTRY_DOCKERHUB="docker.io"
-REGISTRY_QUAY="quay.io"
+REGISTRY="quay.io"
 ORGANIZATION="stackstate"
 
 echo "IMAGE_TAG=${IMAGE_TAG}"
@@ -18,39 +17,37 @@ echo "DOCKERFILE_PATH=${DOCKERFILE_PATH}"
 BUILD_TAG="${IMAGE_REPO}:${IMAGE_TAG}"
 
 # shellcheck disable=SC2154
-docker login -u "${docker_user}" -p "${docker_password}" "${REGISTRY}"
-# shellcheck disable=SC2154
-docker login -u "${quay_user}" -p "${quay_password}" "${REGISTRY_QUAY}"
+docker login -u "${quay_user}" -p "${quay_password}" "${REGISTRY}"
 
 docker build -t "${BUILD_TAG}" "${DOCKERFILE_PATH}"
 
-for REGISTRY in "${REGISTRY_QUAY}" "${REGISTRY_DOCKERHUB}"; do
-    DOCKER_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
 
-    docker tag "${BUILD_TAG}" "${DOCKER_TAG}"
-    docker push "${DOCKER_TAG}"
+DOCKER_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
 
-    if [ -n "$EXTRA_TAG" ]; then
-        DOCKER_EXTRA_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
-        docker tag "${DOCKER_TAG}" "${DOCKER_EXTRA_TAG}"
-        echo "Pushing release to ${EXTRA_TAG}"
-        docker push "${DOCKER_EXTRA_TAG}"
+docker tag "${BUILD_TAG}" "${DOCKER_TAG}"
+docker push "${DOCKER_TAG}"
 
-        # If K8S_REPO is not equal to "NOP" and is set then push the image to the k8s repo
-        if [ -n "${K8S_REPO}" ] && [ "${K8S_REPO}" != "NOP" ]; then
-            DOCKER_K8S_TAG="${REGISTRY}/${ORGANIZATION}/${K8S_REPO}:${EXTRA_TAG}"
-            docker tag "${DOCKER_TAG}" "${DOCKER_K8S_TAG}"
-            echo "Pushing release to ${K8S_REPO}"
-            docker push "${DOCKER_K8S_TAG}"
-        fi
+if [ -n "$EXTRA_TAG" ]; then
+    DOCKER_EXTRA_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+    docker tag "${DOCKER_TAG}" "${DOCKER_EXTRA_TAG}"
+    echo "Pushing release to ${EXTRA_TAG}"
+    docker push "${DOCKER_EXTRA_TAG}"
 
+    # If K8S_REPO is not equal to "NOP" and is set then push the image to the k8s repo
+    if [ -n "${K8S_REPO}" ] && [ "${K8S_REPO}" != "NOP" ]; then
+        DOCKER_K8S_TAG="${REGISTRY}/${ORGANIZATION}/${K8S_REPO}:${EXTRA_TAG}"
+        docker tag "${DOCKER_TAG}" "${DOCKER_K8S_TAG}"
+        echo "Pushing release to ${K8S_REPO}"
+        docker push "${DOCKER_K8S_TAG}"
     fi
-done
+
+fi
+
 
 # Comment out the if and fi lines to test anchore scanning on any branch.
 if [ -n "${CI_COMMIT_TAG}" ] || [ "${CI_COMMIT_BRANCH}" = "master" ]; then
     # for Anchore use publicly accessible image tag
-    DOCKER_TAG="${REGISTRY_DOCKERHUB}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+    DOCKER_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
     echo "Scanning image ${DOCKER_TAG} for vulnerabilities"
     omnibus/package-scripts/anchore_scan.sh -i "${DOCKER_TAG}" -n 0
 fi
