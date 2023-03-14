5082fa91b7055f33ac4a4eb199d86e8146583c35
Melcom van Eeden <melcomve@outlook.com>
2023-03-14 12:46:11 +0100

    STAC-19249 Add pod watch collector (For OOM Monitor Change Events)

--- pkg/util/kubernetes/apiserver/pod.go
+++ pkg/util/kubernetes/apiserver/pod.go
@@ -12,11 +12,16 @@ package apiserver
 
 import (
 	"context"
+	"fmt"
+	"github.com/StackVista/stackstate-agent/pkg/util/log"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"strconv"
+	"time"
 )
 
-// GetPods() retrieves all the pods in the Kubernetes cluster across all namespaces.
+// GetPods retrieves all the pods in the Kubernetes cluster across all namespaces.
 func (c *APIClient) GetPods() ([]v1.Pod, error) {
 	podList, err := c.Cl.CoreV1().Pods(metav1.NamespaceAll).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
@@ -24,4 +29,183 @@ func (c *APIClient) GetPods() ([]v1.Pod, error) {
 	}
 
 	return podList.Items, nil
+}
+
+// RunPodCollection TODO: add comment
+func (c *APIClient) RunPodCollection(resourceVersion string, lastSyncTime time.Time, podReadTimeout int64, podCardinalityLimit int64, resync int64, filter string) ([]*v1.Pod, string, time.Time, error) {
+	// TODO: Change to debug
+	log.Infof("Starting pod collection")
+
+	var pods []*v1.Pod
+
+	// Determine if the resource value is empty or the sync time has expired
+	// If it is then we attempt to reset the resource version
+	syncTimeout := time.Duration(resync) * time.Second
+	syncDiffTime := time.Now().Sub(lastSyncTime)
+	if resourceVersion == "" || syncDiffTime > syncTimeout {
+		// TODO: Change to debug
+		log.Infof("Return listForPodsResync syncDiffTime: %d/%d", syncDiffTime, syncTimeout)
+
+		// Get a new list of pods seeing that the sync time has expired or resourceVersion is empty
+		podList, lastResourceVersion, lastTime, err := c.getListOfPods(podReadTimeout, podCardinalityLimit, filter)
+		if err != nil {
+			return nil, "", time.Now(), err
+		}
+
+		// Convert the resource version to an integer, if it fails then we need to force an integer
+		// to allow further integer operations to determine if the resourceVersion increased
+		resourceVersionInt, ok := strconv.Atoi(resourceVersion)
+		if ok != nil {
+			resourceVersionInt = 0
+		}
+
+		return findPodsAfterResourceVersion(resourceVersionInt, podList), lastResourceVersion, lastTime, nil
+	}
+
+	// Watch pods and trigger a channel event for any pod changes
+	// From the pod events we can start extracting changes like status updates
+	podsWatcher, err := c.Cl.CoreV1().Pods(metav1.NamespaceAll).Watch(context.TODO(), metav1.ListOptions{
+		Watch:           true,
+		ResourceVersion: resourceVersion,
+		Limit:           podCardinalityLimit,
+		FieldSelector:   filter,
+	})
+
+	defer podsWatcher.Stop()
+
+	// If there is an error in the watch event then return all the pods that was already captured within the last cycle
+	if err != nil {
+		return pods, resourceVersion, lastSyncTime, err
+	}
+
+	// TODO: Change to debug
+	log.Infof("Starting to watch pods from %s", resourceVersion)
+
+	timeoutParse := time.NewTimer(time.Duration(podReadTimeout) * time.Second)
+
+	for {
+		select {
+		case podEvent, ok := <-podsWatcher.ResultChan():
+			log.Infof("Received pod watch event: %v", podEvent.Type)
+
+			// If the channel is closed then return the pods that was already captured within the last cycle
+			if !ok {
+				return pods, resourceVersion, lastSyncTime, fmt.Errorf("unexpected channel close while watching pods")
+			}
+
+			switch podEvent.Type {
+			// Determine if an error occurred when receiving the latest pod update from the watch event
+			case watch.Error:
+				// Attempt to extract the status to determine the error status
+				status, ok := podEvent.Object.(*metav1.Status)
+				if !ok {
+					return pods, resourceVersion, lastSyncTime, fmt.Errorf("could not unmarshall the status of the pod watched event")
+				}
+
+				switch status.Reason {
+				case "Expired":
+					// TODO: Change to debug
+					log.Infof("Resource Version is too old, listing all events and collecting only the new ones")
+					podList, resourceVersion, lastListTime, err := c.getListOfPods(podReadTimeout, podCardinalityLimit, filter)
+					if err != nil {
+						return pods, resourceVersion, lastListTime, err
+					}
+
+					resourceVersionInt, err := strconv.Atoi(resourceVersion)
+					if err != nil {
+						_ = log.Errorf("Error converting the stored Resource Version: %s", err.Error())
+						continue
+					}
+
+					return findPodsAfterResourceVersion(resourceVersionInt, podList), resourceVersion, lastListTime, nil
+
+				default:
+					return pods, resourceVersion, lastSyncTime, fmt.Errorf("received an unexpected status while collecting the events: %s", status.Reason)
+				}
+
+			// The events informer sends the state of an object immediately before deletion.
+			// We're not interested in re-processing these events because they should be processed already when they were added.
+			// This happens when an event reaches the events TTL, an apiserver config (default 1 hour).
+			// Ignoring this type of informer events will prevent from sending duplicated datadog events.
+			case watch.Deleted:
+				continue
+
+			default:
+				pod, ok := podEvent.Object.(*v1.Pod)
+				// Could not cast to a pod, might as well drop this pod, and continue.
+				if !ok {
+					_ = log.Errorf("The event object for %v cannot be safely converted, skipping it.", podEvent.Object)
+					continue
+				}
+
+				podResourceVersionInt, err := strconv.Atoi(pod.ResourceVersion)
+				if err != nil {
+					return pods, resourceVersion, lastSyncTime, err
+				}
+
+				pods = append(pods, pod)
+
+				resourceVersionInt, err := strconv.Atoi(resourceVersion)
+				if err != nil {
+					_ = log.Errorf("Could not cast %s into an integer: %s", resourceVersion, err.Error())
+					continue
+				}
+				if podResourceVersionInt > resourceVersionInt {
+					// Events from the watch are not ordered necessarily, let's keep track of the newest RV.
+					resourceVersion = pod.ResourceVersion
+				}
+			}
+
+		case <-timeoutParse.C:
+			// TODO: Change to debug
+			log.Infof("Collected %d pods, will resume watching from resource version %s", len(pods), resourceVersion)
+			// No more events to read or the watch lasted more than `podReadTimeout`.
+			// so return what was processed.
+			return pods, resourceVersion, lastSyncTime, nil
+
+		}
+	}
+}
+
+// findPodsAfterResourceVersion Find all pods that is newer than a specific resource version
+func findPodsAfterResourceVersion(resourceVersionInt int, currentPodList []*v1.Pod) []*v1.Pod {
+	var pods []*v1.Pod
+
+	// Run through the current pod list to determine if the resource version is a valid integer
+	// and if it is valid, make sure that the version is newer than the current resource version
+	for _, pod := range currentPodList {
+		podResourceVersionInt, err := strconv.Atoi(pod.ResourceVersion)
+		if err != nil {
+			_ = log.Errorf("Could not parse resource version of an pod, will skip: %s", err)
+			continue
+		}
+
+		if podResourceVersionInt > resourceVersionInt {
+			pods = append(pods, pod)
+		}
+	}
+
+	// TODO: Change to debug
+	log.Infof("Returning %d pods that we have not collected", len(pods))
+	return pods
+}
+
+// getListOfPods Get the current list of pods
+func (c *APIClient) getListOfPods(timeout int64, limit int64, fieldSelector string) (pods []*v1.Pod, resourceVersion string, lastListTime time.Time, err error) {
+	podList, err := c.Cl.CoreV1().Pods(metav1.NamespaceAll).List(context.TODO(), metav1.ListOptions{
+		TimeoutSeconds: &timeout,
+		Limit:          limit,
+		FieldSelector:  fieldSelector,
+	})
+
+	if err != nil {
+		_ = log.Errorf("Error Listing pods: %s", err.Error())
+		return nil, resourceVersion, lastListTime, err
+	}
+
+	for id := range podList.Items {
+		pods = append(pods, &podList.Items[id])
+	}
+
+	return pods, podList.ResourceVersion, time.Now(), nil
 }
