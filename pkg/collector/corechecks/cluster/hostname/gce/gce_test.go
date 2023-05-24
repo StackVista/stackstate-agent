b69ce270aa999a3cc59527d97450629fa89b5e3a
Jeroen van Erp <jvanerp@stackstate.com>
2023-05-24 20:14:10 +0200

    STAC-19638: Use global hostnames also

--- pkg/collector/corechecks/cluster/hostname/gce/gce_test.go
+++ pkg/collector/corechecks/cluster/hostname/gce/gce_test.go
@@ -1,13 +1,28 @@
 package gce
 
 import (
+	"context"
 	"testing"
 
+	"github.com/StackVista/stackstate-agent/pkg/util/hostname"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGceHostnameUsesProviderID(t *testing.T) {
-	assert.Equal(t, "gke-test-default-pool-9f8f65a4-2kld.europe-west4-a.c.test-stackstate.internal", GetHostname("gce://test-stackstate/europe-west4-a/gke-test-default-pool-9f8f65a4-2kld"))
+const (
+	ZonedHostname  = "my-node.europe-west4-a.c.test-stackstate.internal"
+	GlobalHostname = "my-node.c.test-stackstate.internal"
+)
+
+func TestZonalGceHostnameUsesProviderID(t *testing.T) {
+	withStaticHostnameProvider("gce", ZonedHostname, func() {
+		assert.Equal(t, "gke-test-default-pool-9f8f65a4-2kld.europe-west4-a.c.test-stackstate.internal", GetHostname("gce://test-stackstate/europe-west4-a/gke-test-default-pool-9f8f65a4-2kld"))
+	})
+}
+
+func TestGlobalGceHostnameUsesProviderID(t *testing.T) {
+	withStaticHostnameProvider("gce", GlobalHostname, func() {
+		assert.Equal(t, "gke-test-default-pool-9f8f65a4-2kld.c.test-stackstate.internal", GetHostname("gce://test-stackstate/europe-west4-a/gke-test-default-pool-9f8f65a4-2kld"))
+	})
 }
 
 func TestGceHostnameEmpty(t *testing.T) {
@@ -21,4 +36,13 @@ func TestGceHostnameWrongFormat(t *testing.T) {
 
 func TestGceHostnameWrongPrefix(t *testing.T) {
 	assert.Equal(t, "", GetHostname("abc://test/test"))
+}
+
+func withStaticHostnameProvider(providerID, staticHost string, f func()) {
+	originalHostnameProvider := hostname.GetProvider(providerID)
+	hostname.RegisterHostnameProvider(providerID, func(ctx context.Context, options map[string]interface{}) (string, error) {
+		return staticHost, nil
+	})
+	defer func() { hostname.RegisterHostnameProvider(providerID, originalHostnameProvider) }()
+	f()
 }
