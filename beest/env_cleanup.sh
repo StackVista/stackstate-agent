3b2543be5a87944886db5ea4f3c65a6790675ef4
Daniel Barra <daniel.barra@suse.com>
2025-01-17 10:47:21 -0300

    fix script source

--- beest/env_cleanup.sh
+++ beest/env_cleanup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Start removing terraformLock"
 TERRAFORM_TABLE_NAME=$(aws dynamodb list-tables --output json | jq -r '.TableNames[] | select(contains("beest"))')
 echo $TERRAFORM_TABLE_NAME
 TERRAFORM_LOCK_ID=$(aws dynamodb scan --table-name "$TERRAFORM_TABLE_NAME" --output json | jq -r --arg ci_commit_ref_name "${CI_COMMIT_REF_NAME:0:18}" '.Items[] | select(.LockID.S | contains($ci_commit_ref_name | ascii_downcase)) | .LockID.S')
 echo $TERRAFORM_LOCK_ID
 aws dynamodb delete-item --table-name "$TERRAFORM_TABLE_NAME" --key '{"LockID"':' {"S"':' "'"$TERRAFORM_LOCK_ID"'"}}'
+echo "END"
