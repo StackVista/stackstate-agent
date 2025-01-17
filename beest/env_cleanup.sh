dd1b62078b85d3df2b598cd1d99f9f7fb5e737c3
Daniel Barra <daniel.barra@suse.com>
2025-01-17 14:10:36 -0300

    add commands to clear s3

--- beest/env_cleanup.sh
+++ beest/env_cleanup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+REF_NAME_OBJECTS=$(echo "${CI_COMMIT_REF_NAME:0:18}" | tr '[:upper:]' '[:lower:]')
+echo $REF_NAME_OBJECTS
+
 echo "Start removing terraformLock"
 TERRAFORM_TABLE_NAME=$(aws dynamodb list-tables --output json | jq -r '.TableNames[] | select(contains("beest"))')
 echo $TERRAFORM_TABLE_NAME
-TERRAFORM_LOCK_ID=$(aws dynamodb scan --table-name "$TERRAFORM_TABLE_NAME" --output json | jq -r --arg ci_commit_ref_name "${CI_COMMIT_REF_NAME:0:18}" '.Items[] | select(.LockID.S | contains($ci_commit_ref_name | ascii_downcase)) | .LockID.S')
+TERRAFORM_LOCK_ID=$(aws dynamodb scan --table-name "$TERRAFORM_TABLE_NAME" --output json | jq -r --arg ref_name_objects "$REF_NAME_OBJECTS" '.Items[] | select(.LockID.S | contains($ref_name_objects)) | .LockID.S')
 echo $TERRAFORM_LOCK_ID
 aws dynamodb delete-item --table-name "$TERRAFORM_TABLE_NAME" --key '{"LockID"':' {"S"':' "'"$TERRAFORM_LOCK_ID"'"}}'
+echo "END"
+
+echo "Start clearing s3 bucket"
+S3_BUCKET_NAME=beest-terraform-state
+S3_BUCKET_OBJECT=$(aws s3api list-objects-v2 --bucket $S3_BUCKET_NAME | jq -r --arg ref_name_objects "$REF_NAME_OBJECTS" '.Contents[] | select(.Key | contains($ref_name_objects)) | .Key')
+echo $S3_BUCKET_OBJECT
+aws s3 rm s3://$S3_BUCKET_NAME/$S3_BUCKET_OBJECT
 echo "END"
