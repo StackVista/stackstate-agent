b3ad77e78d3204a100fadef686f87c85aa678767
Louis Parkin <louis.parkin@suse.com>
2025-05-22 09:28:36 +0200

    STAC-22237 Ignore all but k8s, use docker as fallback.

--- Dockerfiles/agent/entrypoint.sh
+++ Dockerfiles/agent/entrypoint.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
-for file in $(ls etc/cont-init.d/ | grep -v "50-ci.sh"); do
+
+FILES="50-kubernetes.sh 51-docker.sh 59-defaults.sh 60-network-check.sh 60-sysprobe-check.sh 89-copy-customfiles.sh"
+
+for file in ${FILES}; do
     if [[ -f "etc/cont-init.d/${file}" ]]; then
         "etc/cont-init.d/${file}"
         RC=$?
         if [[ $RC -ne 0 ]]; then
             echo "Error in ${file} with return code ${RC}"
-            exit $RC
+#            exit $RC
         fi
     fi
 done
+
+if [[ ! -e /etc/stackstate-agent/stackstate.yaml ]]; then
+    echo "Despite our best efforts, we could not find a config file to use."
+    echo "Please mount a config file to /etc/stackstate-agent/stackstate.yaml"
+    echo "or set the STS_CONFIG_FILE environment variable to a config file."
+    echo "Exiting..."
+    sleep 20
+    exit 1
+fi
 
 /opt/stackstate-agent/bin/agent/agent run
