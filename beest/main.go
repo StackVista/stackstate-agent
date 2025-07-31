e699f54d6d793b4622d508fea26b489ae967fd60
Daniel Barra <daniel.barra@suse.com>
2025-07-31 10:41:14 -0300

    STAC-23156: Remove all files related with beest

--- beest/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "beest/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- beest/testframework/stscliv1/stscliv1/__init__.py
+++
@@ -1,2 +0,0 @@
-from .models import TopologyResult, ComponentWrapper, RelationWrapper
-from .cliv1 import CLIv1
