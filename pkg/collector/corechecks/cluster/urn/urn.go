d04ef13ef40106743311c5352349341028193e75
Bram Schuur <bschuur@stackstate.com>
2025-08-04 16:32:36 +0200

    STAC-23081: Fix identifier for projected volume

--- pkg/collector/corechecks/cluster/urn/urn.go
+++ pkg/collector/corechecks/cluster/urn/urn.go
@@ -39,6 +39,7 @@ type Builder interface {
 	BuildIngressExternalID(namespace, ingressName string) string
 	BuildVolumeExternalID(namespace, volumeName string) string
 	BuildExternalVolumeExternalID(volumeType string, volumeComponents ...string) string
+	BuildLocalExternalVolumeExternalID(volumeType string, volumeComponents ...string) string
 	BuildPersistentVolumeExternalID(persistentVolumeName string) string
 	BuildPersistentVolumeClaimExternalID(namespace, persistentVolumeName string) string
 	BuildComponentExternalID(component, namespace, name string) string
