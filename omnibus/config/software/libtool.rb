66a81a9c02689a86d7244a27235e2be78bdffe1a
Louis Parkin <louis.parkin@suse.com>
2025-09-15 11:38:40 +0200

    STAC-23355 Attempt to work around the libtool network error.
    https://gitlab.com/stackvista/agent/stackstate-agent/-/jobs/11354021321
    
    STAC-23355 Attempt to work around the libtool network error. [2]
    
    STAC-23355 Libtool just doesn't want to stay downloadable.  Yet another mirror.

---
+++ omnibus/config/software/libtool.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+# Copyright:: Your Company
+
+name "libtool"
+default_version "2.4.7"
+
+license "GPL-2.0-or-later"
+license_file "COPYING"
+skip_transitive_dependency_licensing true
+
+# Pin checksums per version to satisfy Omnibus security checks
+version("2.4.7") { source sha256: "4f7f217f057ce655ff22559ad221a0fd8ef84ad1fc5fcb6990cecc333aa1635d" }
+
+# Use a stable mirror for libtool source
+source url: "https://mirror.easyname.at/gnu/libtool/libtool-#{version}.tar.xz"
+
+relative_path "libtool-#{version}"
+
+dependency "m4"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  configure_options = [
+    "--disable-nls",
+  ]
+
+  configure(*configure_options, env: env)
+  make "-j #{workers}", env: env
+  make "-j #{workers} install", env: env
+end
+
+
