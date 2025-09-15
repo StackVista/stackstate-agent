50e3b94d9608f1bd4348e9d9886b0237e06a10c5
Louis Parkin <louis.parkin@suse.com>
2025-09-15 13:10:11 +0200

    STAC-23355 Attempt to work around the libtool network error. [2]

--- omnibus/config/software/libtool.rb
+++ omnibus/config/software/libtool.rb
@@ -9,9 +9,11 @@ license "GPL-2.0-or-later"
 license_file "COPYING"
 skip_transitive_dependency_licensing true
 
-# Prefer the GNU mirror redirector to avoid flaky single-host timeouts
-# You can pin a checksum by adding a version block with `source sha256:` if desired.
-source url: "https://ftpmirror.gnu.org/libtool/libtool-#{version}.tar.xz"
+# Pin checksums per version to satisfy Omnibus security checks
+version("2.4.7") { source sha256: "4f7f217f057ce655ff22559ad221a0fd8ef84ad1fc5fcb6990cecc333aa1635d" }
+
+# Use a stable mirror for libtool source
+source url: "https://mirror.clientvps.com/gnu/libtool/libtool-#{version}.tar.xz"
 
 relative_path "libtool-#{version}"
 
